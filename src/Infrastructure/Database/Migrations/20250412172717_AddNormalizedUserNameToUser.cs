5e848604fa96d4be59820c5932c5e68ce52aa729
Erfan <ErfanMelon@outlook.com>
2025-04-12 22:08:54 +0330

    feat: Implement migration for NormalizedUserName

---
+++ src/Infrastructure/Database/Migrations/20250412172717_AddNormalizedUserNameToUser.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Database.Migrations;
+
+/// <inheritdoc />
+public partial class AddNormalizedUserNameToUser : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropIndex(
+            name: "ix_users_user_name",
+            schema: "public",
+            table: "users");
+
+        migrationBuilder.AddColumn<string>(
+            name: "normalized_user_name",
+            schema: "public",
+            table: "users",
+            type: "TEXT",
+            nullable: false,
+            defaultValue: "");
+
+        migrationBuilder.CreateIndex(
+            name: "ix_users_normalized_user_name",
+            schema: "public",
+            table: "users",
+            column: "normalized_user_name");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropIndex(
+            name: "ix_users_normalized_user_name",
+            schema: "public",
+            table: "users");
+
+        migrationBuilder.DropColumn(
+            name: "normalized_user_name",
+            schema: "public",
+            table: "users");
+
+        migrationBuilder.CreateIndex(
+            name: "ix_users_user_name",
+            schema: "public",
+            table: "users",
+            column: "user_name",
+            unique: true);
+    }
+}
