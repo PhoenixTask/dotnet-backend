200a41b3ba663ccec66e138d7374b5c5cbbb5133
Erfan <ErfanMelon@outlook.com>
2025-04-12 22:08:28 +0330

    ref: Improve User entity configuration

--- src/Infrastructure/Users/UserConfiguration.cs
+++ src/Infrastructure/Users/UserConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Users;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Users;
@@ -11,7 +12,9 @@ internal sealed class UserConfiguration : CommonEntityConfiguration<User>
 
         builder.HasIndex(u => u.Email).IsUnique();
 
-        builder.HasIndex(u => u.UserName).IsUnique();
+        builder.Property(u=>u.UserName).IsRequired();
+
+        builder.HasIndex(u => u.NormalizedUserName);
 
         base.Configure(builder);
     }
