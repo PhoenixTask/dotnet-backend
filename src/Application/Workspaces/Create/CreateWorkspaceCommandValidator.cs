79da03333eae7367fd4cc4d7d7561e87e6f79b49
Erfan <ErfanMelon@outlook.com>
2025-04-13 22:31:42 +0330

    feat: Add validation for CreateWorkspaceCommand

---
+++ src/Application/Workspaces/Create/CreateWorkspaceCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Workspaces.Create;
+
+internal sealed class CreateWorkspaceCommandValidator : AbstractValidator<CreateWorkspaceCommand>
+{
+    public CreateWorkspaceCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Color).NotEmpty();
+    }
+}
