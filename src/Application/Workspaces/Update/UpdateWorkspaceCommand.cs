47c909ebe2ad3dd08e6075e0b4fca0d6c2a6d75c
Erfan <ErfanMelon@outlook.com>
2025-04-13 22:32:18 +0330

    feat: Add UpdateWorkspaceCommand

---
+++ src/Application/Workspaces/Update/UpdateWorkspaceCommand.cs
@@ -0,0 +1,5 @@
+﻿using Application.Abstractions.Messaging;
+using SharedKernel;
+
+namespace Application.Workspaces.Update;
+public sealed record UpdateWorkspaceCommand(Guid Id , string Name , string Color) : ICommand<Result>;
