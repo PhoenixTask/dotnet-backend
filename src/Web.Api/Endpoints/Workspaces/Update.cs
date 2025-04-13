024232d269aad8700f3ede368d74cb4512442b0f
Erfan <ErfanMelon@outlook.com>
2025-04-13 22:33:05 +0330

    feat: Add endpoint for updating workspaces

---
+++ src/Web.Api/Endpoints/Workspaces/Update.cs
@@ -0,0 +1,29 @@
+﻿using Application.Workspaces.Create;
+using Application.Workspaces.Update;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Workspaces;
+
+internal sealed class Update : IEndpoint
+{
+    public sealed record Request(string Name,string Color);
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPut("workspace/update/{Id}", async (Guid Id , Request request ,ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new UpdateWorkspaceCommand(
+                Id,
+                request.Name,
+                request.Color);
+
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Workspaces);
+    }
+}
