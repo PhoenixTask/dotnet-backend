94c5b04af51fbd2507dee34d913a237683919665
Erfan <ErfanMelon@outlook.com>
2025-04-13 22:29:16 +0330

    feat: Add WorkspaceErrors class

---
+++ src/Domain/Workspaces/WorkspaceErrors.cs
@@ -0,0 +1,9 @@
+﻿using SharedKernel;
+
+namespace Domain.Workspaces;
+public static class WorkspaceErrors
+{
+    public static Error NotFound(Guid workspaceId) => Error.NotFound(
+        "Workspace.NotFound",
+        $"The workspace with the Id = '{workspaceId}' was not found");
+}
