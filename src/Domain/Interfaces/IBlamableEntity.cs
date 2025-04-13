27bd5c70cce0a3521c76d1f9987e3af70fbcee31
Erfan <ErfanMelon@outlook.com>
2025-04-13 22:28:06 +0330

    feat: Add CreatedById and ModifiedById to IBlamableEntity

--- src/Domain/Interfaces/IBlamableEntity.cs
+++ src/Domain/Interfaces/IBlamableEntity.cs
@@ -4,5 +4,7 @@ namespace Domain.Interfaces;
 public interface IBlamableEntity
 {
     User? CreatedBy { get; }
+    Guid? CreatedById { get; }
     User? ModifiedBy { get; }
+    Guid? ModifiedById { get; }
 }
