8918de039fc56ee0cfeab221215a4d02b0b15572
Erfan <ErfanMelon@outlook.com>
2025-04-13 22:31:59 +0330

    feat: Implement workspace creation command handler

---
+++ src/Application/Workspaces/Create/CreateWorkspaceCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Domain.Workspaces;
+using SharedKernel;
+
+namespace Application.Workspaces.Create;
+
+internal sealed class CreateWorkspaceCommandHandler(IApplicationDbContext context)
+    : ICommandHandler<CreateWorkspaceCommand, Guid>
+{
+    public async Task<Result<Guid>> Handle(CreateWorkspaceCommand request, CancellationToken cancellationToken)
+    {
+        var workspace = new Workspace
+        {
+            Name = request.Name.Trim(),
+            Color = request.Color.Trim(),
+        };
+        await context.Workspaces.AddAsync(workspace, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+        return workspace.Id;
+    }
+}
