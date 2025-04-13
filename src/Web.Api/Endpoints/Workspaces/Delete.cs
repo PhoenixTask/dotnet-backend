9f41af60d874ac1c2bf2df0e22ec58f0f785c7ee
Erfan <ErfanMelon@outlook.com>
2025-04-13 22:33:28 +0330

    feat: Add endpoint for deleting workspaces

---
+++ src/Web.Api/Endpoints/Workspaces/Delete.cs
@@ -0,0 +1,25 @@
+﻿using Application.Workspaces.Create;
+using Application.Workspaces.Delete;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Workspaces;
+
+internal sealed class Delete : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("workspace/delete/{id}", async (Guid id,ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new DeleteWorkspaceCommand(id);
+
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Workspaces);
+    }
+}
