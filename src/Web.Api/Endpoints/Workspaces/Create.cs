830c7447e7b8c642b5708dea95967654229ba063
Erfan <ErfanMelon@outlook.com>
2025-04-13 22:32:07 +0330

    feat: Add Create Workspace endpoint

---
+++ src/Web.Api/Endpoints/Workspaces/Create.cs
@@ -0,0 +1,27 @@
+﻿using Application.Workspaces.Create;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Workspaces;
+
+internal sealed class Create : IEndpoint
+{
+    public sealed record Request(string Name,string Color);
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("workspace/create/", async (Request request ,ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new CreateWorkspaceCommand(
+                request.Name,
+                request.Color);
+
+            Result<Guid> result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Workspaces);
+    }
+}
