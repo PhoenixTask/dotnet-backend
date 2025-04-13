7839901dcc0ee4757a4ce4e0b2407a8469ba70bb
Erfan <ErfanMelon@outlook.com>
2025-04-13 22:29:44 +0330

    feat: Add WorkspaceConfiguration class

---
+++ src/Infrastructure/Workspaces/WorkspaceConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Workspaces;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Workspaces;
+internal sealed class WorkspaceConfiguration : CommonEntityConfiguration<Workspace>
+{
+    public override void Configure(EntityTypeBuilder<Workspace> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        base.Configure(builder);
+    }
+}
