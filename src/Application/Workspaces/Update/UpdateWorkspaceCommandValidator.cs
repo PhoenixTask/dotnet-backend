116f23a135b1225e9ad316aa3ec7cad323dc6075
Erfan <ErfanMelon@outlook.com>
2025-04-13 22:32:41 +0330

    feat: Add FluentValidation to UpdateWorkspaceCommand

---
+++ src/Application/Workspaces/Update/UpdateWorkspaceCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Workspaces.Update;
+
+internal sealed class UpdateWorkspaceCommandValidator : AbstractValidator<UpdateWorkspaceCommand>
+{
+    public UpdateWorkspaceCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty();
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Color).NotEmpty();
+    }
+}
