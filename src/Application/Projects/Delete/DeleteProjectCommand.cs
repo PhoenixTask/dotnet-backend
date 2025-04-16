2e10624ab0cb22e661b5efc84d69fd3e61266662
ErfanMelon <ErfanMelon@outlook.com>
2025-04-16 21:05:03 +0330

    feat: add delete project command

---
+++ src/Application/Projects/Delete/DeleteProjectCommand.cs
@@ -0,0 +1,5 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Projects.Delete;
+
+public sealed record DeleteProjectCommand(Guid ProjectId) : ICommand;
---
+++ src/Application/Projects/Delete/DeleteProjectCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Domain.Projects;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Application.Projects.Delete;
+
+internal sealed class DeleteProjectCommandHandler(
+    IApplicationDbContext context, IUserContext userContext) : ICommandHandler<DeleteProjectCommand>
+{
+    public async Task<Result> Handle(DeleteProjectCommand request, CancellationToken cancellationToken)
+    {
+        Guid userId = userContext.UserId;
+
+        int countDeleted = await context.Projects
+            .Where(x => x.Id == request.ProjectId && x.CreatedById == userId)
+            .ExecuteDeleteAsync(cancellationToken);
+
+        return countDeleted > 0 ? Result.Success() : Result.Failure(ProjectErrors.NotFound(request.ProjectId))
+    }
+}
