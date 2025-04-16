98c45cab55c2e2fc39c13ac2c7dac426dab3d8c2
Erfan <ErfanMelon@outlook.com>
2025-04-16 22:23:11 +0330

    feat: Add BoardErrors class

---
+++ src/Domain/Projects/BoardErrors.cs
@@ -0,0 +1,9 @@
+﻿using SharedKernel;
+
+namespace Domain.Projects;
+public static class BoardErrors
+{
+    public static Error NotFound(Guid boardId) => Error.NotFound(
+        "Board.NotFound",
+        $"The board with the Id = '{boardId}' was not found");
+}
