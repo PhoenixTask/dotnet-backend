a11cc177e5d1a4917cdf1bdc4af0ba5ced4b988e
Erfan <ErfanMelon@outlook.com>
2025-04-16 23:14:24 +0330

    feat: Add CreateTaskCommandValidator

---
+++ src/Application/Tasks/Create/CreateTaskCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Tasks;
+using FluentValidation;
+
+namespace Application.Tasks.Create;
+
+internal sealed class CreateTaskCommandValidator : AbstractValidator<CreateTaskCommand>
+{
+    public CreateTaskCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.BoardId).NotEmpty();
+        RuleFor(x => x.Description).MaximumLength(225);
+        RuleFor(x => x.DeadLine).GreaterThanOrEqualTo(DateTime.Now).WithErrorCode(TaskErrors.ExpiredDeadLine.Code).WithMessage(TaskErrors.ExpiredDeadLine.Description);
+        RuleFor(x => x.Order).ExclusiveBetween(-100, 100);
+        RuleFor(x => x.Priority).ExclusiveBetween(-100, 100);
+    }
+}
