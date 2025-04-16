721330df909199dfccfdb571728ffcca7e4aeb4f
Erfan <ErfanMelon@outlook.com>
2025-04-16 22:24:10 +0330

    feat: Add Update Board endpoint

---
+++ src/Web.Api/Endpoints/Boards/Update.cs
@@ -0,0 +1,29 @@
+﻿using Application.Boards.Update;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Boards;
+
+internal sealed class Update : IEndpoint
+{
+    public sealed record Request(string Name, string Color, int Order);
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPut("board/update/{Id}", async (Guid Id , Request request ,ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new UpdateBoardCommand(
+               Id,
+               request.Name,
+               request.Color,
+               request.Order);
+
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Boards);
+    }
+}
