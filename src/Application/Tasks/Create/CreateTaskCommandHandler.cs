6acc39a22648ae6579270259d21dd92afb2618da
Erfan <ErfanMelon@outlook.com>
2025-04-16 23:14:32 +0330

    feat: Add CreateTaskCommandHandler

---
+++ src/Application/Tasks/Create/CreateTaskCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Domain.Projects;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+using Task = Domain.Tasks.Task;
+
+namespace Application.Tasks.Create;
+
+internal sealed class CreateTaskCommandHandler(
+    IApplicationDbContext context , IUserContext userContext) : ICommandHandler<CreateTaskCommand, Guid>
+{
+    public async Task<Result<Guid>> Handle(CreateTaskCommand request, CancellationToken cancellationToken)
+    {
+        Guid userId = userContext.UserId;
+        Board? board = await context.Boards
+            .SingleOrDefaultAsync(x => x.Id == request.BoardId && x.CreatedById == userId, cancellationToken: cancellationToken);
+        
+        if(board is null)
+        {
+            return Result.Failure<Guid>(BoardErrors.NotFound(request.BoardId));
+        }
+
+        var deadLine = DateOnly.FromDateTime(request.DeadLine);
+
+        var task = new Task
+        {
+            Board = board,
+            DeadLine = deadLine,
+            Description = request.Description,
+            Name = request.Name,
+            Order = request.Order,
+            Priority = request.Priority,
+        };
+
+        context.Tasks.Add(task);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return task.Id;
+    }
+}
