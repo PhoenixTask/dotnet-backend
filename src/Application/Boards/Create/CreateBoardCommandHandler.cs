c48cc4ff1d42c683a38d702d01715514c35892bb
Erfan <ErfanMelon@outlook.com>
2025-04-16 22:12:01 +0330

    feat: Add CreateBoardCommandHandler

---
+++ src/Application/Boards/Create/CreateBoardCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Domain.Projects;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Application.Boards.Create;
+
+internal sealed class CreateBoardCommandHandler(
+    IApplicationDbContext context, IUserContext userContext) : ICommandHandler<CreateBoardCommand, Guid>
+{
+    public async Task<Result<Guid>> Handle(CreateBoardCommand request, CancellationToken cancellationToken)
+    {
+        Guid userId = userContext.UserId;
+        Project? project = await context.Projects.SingleOrDefaultAsync(x => x.Id == request.ProjectId && x.CreatedById == userId, cancellationToken: cancellationToken);
+
+        if (project is null)
+        {
+            return Result.Failure<Guid>(ProjectErrors.NotFound(request.ProjectId));
+        }
+
+        var board = new Board
+        {
+            Color = request.Color,
+            Order = request.Order,
+            IsArchive = false,
+            Name = request.Name,
+            Project = project
+        };
+
+        context.Boards.Add(board);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return board.Id;
+    }
+}
