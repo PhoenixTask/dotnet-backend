d366c9b7f6e01865d2bf4c810fb5ffc2bcb73880
Erfan <ErfanMelon@outlook.com>
2025-04-16 22:23:25 +0330

    feat: Add UpdateBoardCommand

---
+++ src/Application/Boards/Update/UpdateBoardCommand.cs
@@ -0,0 +1,4 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Boards.Update;
+public sealed record UpdateBoardCommand(Guid BoardId, string Name, string Color, int Order) : ICommand;
