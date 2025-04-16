fa364bad4b8132667394747d8f1fca40827d09c0
Erfan <ErfanMelon@outlook.com>
2025-04-16 23:15:04 +0330

    feat: Add GetTasksQuery

---
+++ src/Application/Tasks/Get/GetTasksQuery.cs
@@ -0,0 +1,4 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Tasks.Get;
+public sealed record GetTasksQuery(int Page, int PageSize, Guid BoardId) : IQuery<List<TaskResponse>>;
