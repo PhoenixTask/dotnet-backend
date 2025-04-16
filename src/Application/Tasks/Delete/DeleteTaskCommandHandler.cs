5e607c9b420d8c9f78a5dad8f471887413112d04
Erfan <ErfanMelon@outlook.com>
2025-04-16 23:14:47 +0330

    feat: Add DeleteTaskCommandHandler

---
+++ src/Application/Tasks/Delete/DeleteTaskCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Domain.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Application.Tasks.Delete;
+
+internal sealed class DeleteTaskCommandHandler(
+    IApplicationDbContext context, IUserContext userContext) : ICommandHandler<DeleteTaskCommand>
+{
+    public async Task<Result> Handle(DeleteTaskCommand request, CancellationToken cancellationToken)
+    {
+        Guid userId = userContext.UserId;
+
+        int countDeleted = await context.Tasks
+            .Where(x => x.Id == request.TaskId && x.CreatedById == userId)
+            .ExecuteDeleteAsync(cancellationToken);
+
+        return countDeleted > 0 ? Result.Success() : Result.Failure(TaskErrors.NotFound(request.TaskId));
+    }
+}
