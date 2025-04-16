73cccd167656273437dc1757a403b87211e88045
Erfan <ErfanMelon@outlook.com>
2025-04-16 22:23:48 +0330

    feat: Add Boards tag to Tags.cs

--- src/Web.Api/Endpoints/Tags.cs
+++ src/Web.Api/Endpoints/Tags.cs
@@ -5,4 +5,5 @@ public static class Tags
     public const string Users = "Users";
     public const string Workspaces = "Workspaces";
     public const string Projects = "Projects";
+    public const string Boards = "Boards";
 }
