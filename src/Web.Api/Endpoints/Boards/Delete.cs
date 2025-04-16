7e602f1a5ce761ccff2325f7ae6bf795bc0266ff
Erfan <ErfanMelon@outlook.com>
2025-04-16 22:23:58 +0330

    feat: Add endpoint to delete boards

---
+++ src/Web.Api/Endpoints/Boards/Delete.cs
@@ -0,0 +1,24 @@
+﻿using Application.Boards.Delete;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Boards;
+
+internal sealed class Delete : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("board/delete/{id}", async (Guid id,ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new DeleteBoardCommand(id);
+
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Boards);
+    }
+}
