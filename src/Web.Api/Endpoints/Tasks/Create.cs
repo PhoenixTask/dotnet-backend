fa50123bd8ed797f36ec0dc055854c7fe20d16ac
Erfan <ErfanMelon@outlook.com>
2025-04-16 23:15:41 +0330

    feat: Add Create Task endpoint

---
+++ src/Web.Api/Endpoints/Tasks/Create.cs
@@ -0,0 +1,33 @@
+﻿using Application.Tasks.Create;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Tasks;
+
+internal sealed class Create : IEndpoint
+{
+    public sealed record Request(string Name, Guid BoardId, string Description, DateTime DeadLine, int Order, int Priority);
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("task/create/", async (Request request, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new CreateTaskCommand
+            {
+                BoardId = request.BoardId,
+                Priority = request.Priority,
+                Order = request.Order,
+                Name = request.Name,
+                Description = request.Description,
+                DeadLine = request.DeadLine
+            };
+
+            Result<Guid> result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Tasks);
+    }
+}
