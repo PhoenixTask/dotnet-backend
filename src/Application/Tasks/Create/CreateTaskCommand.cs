e63762493576406a733fb5ba3efb5dc67fe6fe47
Erfan <ErfanMelon@outlook.com>
2025-04-16 23:14:15 +0330

    feat: Add CreateTaskCommand

---
+++ src/Application/Tasks/Create/CreateTaskCommand.cs
@@ -0,0 +1,12 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Tasks.Create;
+public sealed class CreateTaskCommand : ICommand<Guid>
+{
+    public string Name { get; set; }
+    public Guid BoardId { get; set; }
+    public string Description { get; set; }
+    public DateTime DeadLine { get; set; }
+    public int Order { get; set; }
+    public int Priority { get; set; }
+}
