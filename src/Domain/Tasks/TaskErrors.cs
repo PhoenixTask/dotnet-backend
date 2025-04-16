fb8c623e48e025731ae3117324e12c385d3203c5
Erfan <ErfanMelon@outlook.com>
2025-04-16 23:13:58 +0330

    feat: Add TaskErrors class

---
+++ src/Domain/Tasks/TaskErrors.cs
@@ -0,0 +1,12 @@
+﻿using SharedKernel;
+
+namespace Domain.Tasks;
+public static class TaskErrors
+{
+    public static Error NotFound(Guid taskId) => Error.NotFound(
+        "Task.NotFound",
+        $"The task with the Id = '{taskId}' was not found");
+    public static readonly Error ExpiredDeadLine = Error.Failure(
+        "Task.ExpiredDeadLine",
+        $"The deadline is passed and cannot be assigned");
+}
