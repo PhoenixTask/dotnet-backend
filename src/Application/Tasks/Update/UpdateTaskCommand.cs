e02fcf2103784685922cdce1c042a056c088316f
Erfan <ErfanMelon@outlook.com>
2025-04-16 23:15:22 +0330

    feat: Add UpdateTaskCommand

---
+++ src/Application/Tasks/Update/UpdateTaskCommand.cs
@@ -0,0 +1,13 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Tasks.Update;
+public sealed class UpdateTaskCommand : ICommand
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public Guid BoardId { get; set; }
+    public string Description { get; set; }
+    public DateTime DeadLine { get; set; }
+    public int Order { get; set; }
+    public int Priority { get; set; }
+}
