38f3448cefc38f487d5a0a5f2b0e95a5bd762cb2
Erfan <ErfanMelon@outlook.com>
2025-04-16 23:15:48 +0330

    feat: Add endpoint for deleting tasks

---
+++ src/Web.Api/Endpoints/Tasks/Delete.cs
@@ -0,0 +1,24 @@
+﻿using Application.Tasks.Delete;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Tasks;
+
+internal sealed class Delete : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("task/delete/{id}", async (Guid id,ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new DeleteTaskCommand(id);
+
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Tasks);
+    }
+}
