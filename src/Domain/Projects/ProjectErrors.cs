350cebf440f0e44890dde704c366b238bd0ea63d
ErfanMelon <ErfanMelon@outlook.com>
2025-04-16 21:03:39 +0330

    feat: add project errors

---
+++ src/Domain/Projects/ProjectErrors.cs
@@ -0,0 +1,10 @@
+﻿using SharedKernel;
+
+namespace Domain.Projects;
+
+public static class ProjectErrors
+{
+    public static Error NotFound(Guid projectId) => Error.NotFound(
+        "Project.NotFound",
+        $"The project with the Id = '{projectId}' was not found");
+}
