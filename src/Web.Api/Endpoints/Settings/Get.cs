2a7686fa6e7cc2eee650395d291bf44bf6bd9ba7
ErfanMelon <ErfanMelon@outlook.com>
2025-04-19 12:44:52 +0330

    feat: Add endpoints for retrieving user settings

---
+++ src/Web.Api/Endpoints/Settings/Get.cs
@@ -0,0 +1,36 @@
+﻿using Application.Users.GetSetting;
+using Application.Users.GetSettings;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Settings;
+
+internal sealed class Get : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("user/setting/{Key}", async (string Key, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var query = new GetSettingQuery(Key);
+
+            Result<string> result = await sender.Send(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Settings);
+
+        app.MapGet("user/setting", async (ISender sender, CancellationToken cancellationToken) =>
+        {
+            var query = new GetSettingsQuery();
+
+            Result<Dictionary<string, string>> result = await sender.Send(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Settings);
+    }
+}
