bc72b546a26ccea71ff300af09b341819f314d59
ErfanMelon <ErfanMelon@outlook.com>
2025-04-19 12:43:29 +0330

    feat: Add GetSettingsQueryHandler to retrieve user settings

---
+++ src/Application/Users/GetSettings/GetSettingsQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Application.Users.GetSettings;
+
+internal sealed class GetSettingsQueryHandler(
+    IApplicationDbContext context, IUserContext userContext) : IQueryHandler<GetSettingsQuery, Dictionary<string, string>>
+{
+    public async Task<Result<Dictionary<string, string>>> Handle(GetSettingsQuery request, CancellationToken cancellationToken)
+    {
+        Guid userId = userContext.UserId;
+        Dictionary<string, string> settings =await context.Settings
+            .Where(x => x.CreatedById == userId).ToDictionaryAsync(x=>x.Key,x=>x.Value,cancellationToken);
+        return settings;
+    }
+}
