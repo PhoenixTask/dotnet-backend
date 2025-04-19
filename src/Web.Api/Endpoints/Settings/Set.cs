30c728a0fa9794f53ace48af90029ea49cca73da
ErfanMelon <ErfanMelon@outlook.com>
2025-04-19 12:45:05 +0330

    feat: Add endpoint for setting user settings

---
+++ src/Web.Api/Endpoints/Settings/Set.cs
@@ -0,0 +1,27 @@
+﻿using Application.Users.SetSetting;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Settings;
+
+internal sealed class Set : IEndpoint
+{
+    public sealed record Request(string Key, string Value);
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("user/setting/", async (Request request, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new SetSettingCommand(
+                request.Key,
+                request.Value);
+
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Workspaces);
+    }
+}
