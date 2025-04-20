59890ba969e118d4b4f6314cc9d39178be764061
Erfan <ErfanMelon@outlook.com>
2025-04-20 19:40:16 +0330

    feat: Add TeamMemberErrors class

---
+++ src/Domain/Subscriptions/TeamMemberErrors.cs
@@ -0,0 +1,9 @@
+﻿using SharedKernel;
+
+namespace Domain.Subscriptions;
+public static class TeamMemberErrors
+{
+    public static readonly Error AlreadyMember = Error.Conflict(
+        "TeamMember.AlreadyMember",
+        "The user already participant in this workspace");
+}
