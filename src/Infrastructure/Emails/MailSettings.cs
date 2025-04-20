aec06aa8ce43efb3a6d46c320528d829507e6262
Erfan <ErfanMelon@outlook.com>
2025-04-20 19:40:53 +0330

    feat: Add MailSettings class

---
+++ src/Infrastructure/Emails/MailSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace Infrastructure.Emails;
+
+public class MailSettings
+{
+    public const string SettingsKey = "Mail";
+    public string SenderDisplayName { get; set; }
+
+    public string SenderEmail { get; set; }
+
+    public string SmtpPassword { get; set; }
+
+    public string SmtpServer { get; set; }
+
+    public int SmtpPort { get; set; }
+}
