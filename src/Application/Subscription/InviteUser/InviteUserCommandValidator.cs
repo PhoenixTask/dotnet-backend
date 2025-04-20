c644788d9e8c025d900f335bf45eafb469a263a4
Erfan <ErfanMelon@outlook.com>
2025-04-20 19:39:53 +0330

    feat: Add InviteUserCommandValidator

---
+++ src/Application/Subscription/InviteUser/InviteUserCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Subscription.InviteUser;
+
+internal sealed class InviteUserCommandValidator : AbstractValidator<InviteUserCommand>
+{
+    public InviteUserCommandValidator()
+    {
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        RuleFor(x => x.WorkspaceId).NotEmpty();
+    }
+}
