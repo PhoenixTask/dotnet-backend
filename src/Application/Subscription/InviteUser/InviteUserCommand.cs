346b74ea6ba76894206983025f2287f91d0bfe6f
Erfan <ErfanMelon@outlook.com>
2025-04-20 19:39:43 +0330

    feat: Add InviteUserCommand

---
+++ src/Application/Subscription/InviteUser/InviteUserCommand.cs
@@ -0,0 +1,5 @@
+﻿using Application.Abstractions.Messaging;
+using Domain.Subscriptions;
+
+namespace Application.Subscription.InviteUser;
+public sealed record InviteUserCommand(string Email , Guid WorkspaceId,ProjectRole Role) : ICommand;
