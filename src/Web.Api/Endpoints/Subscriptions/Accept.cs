b519cecc9aa64d074a8e85b9ddc45b07fbdcb25d
Erfan <ErfanMelon@outlook.com>
2025-04-20 19:41:04 +0330

    feat: Add endpoint for accepting subscription invites

---
+++ src/Web.Api/Endpoints/Subscriptions/Accept.cs
@@ -0,0 +1,25 @@
+﻿
+using Application.Subscription.AcceptInvite;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Subscriptions;
+
+internal sealed class Accept : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("workspace/invite/accept/{token}", async (string token, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new AcceptInviteCommand(token);
+
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+       .RequireAuthorization()
+       .WithTags(Tags.Subscriptions);
+    }
+}
