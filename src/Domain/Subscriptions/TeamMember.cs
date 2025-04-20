4fa14a71ffb7f12944c0c22722e95830e4fbdd28
Erfan <ErfanMelon@outlook.com>
2025-04-20 19:40:12 +0330

    feat: Add TeamMember entity

---
+++ src/Domain/Subscriptions/TeamMember.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Interfaces;
+using Domain.Users;
+using Domain.Workspaces;
+using SharedKernel;
+
+namespace Domain.Subscriptions;
+public sealed class TeamMember : Entity, IAuditableEntity, ISoftDeletableEntity
+{
+    public User User { get; set; }
+    public Guid UserId { get; set; }
+    public ProjectRole Role { get; set; }
+    public Workspace Workspace { get; set; }
+    public Guid WorkspaceId { get; set; }
+    public DateTime? CreatedOnUtc { get; }
+    public DateTime? ModifiedOnUtc { get; }
+    public DateTime? DeletedOnUtc { get; }
+    public bool Deleted { get; }
+    public User? DeletedBy { get; }
+}
