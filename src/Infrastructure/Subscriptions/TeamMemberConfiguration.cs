e2121a3a5c9da5e1cea55b971874b7c3260d3c2b
Erfan <ErfanMelon@outlook.com>
2025-04-20 19:40:45 +0330

    feat: Add TeamMember entity configuration

---
+++ src/Infrastructure/Subscriptions/TeamMemberConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Subscriptions;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Subscriptions;
+internal sealed class TeamMemberConfiguration : CommonEntityConfiguration<TeamMember>
+{
+    public override void Configure(EntityTypeBuilder<TeamMember> builder)
+    {
+        builder.HasKey(x => new { x.UserId, x.WorkspaceId });
+
+        builder.HasOne(x => x.User)
+            .WithMany()
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(Microsoft.EntityFrameworkCore.DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.Workspace)
+            .WithMany()
+            .HasForeignKey(x => x.WorkspaceId)
+            .OnDelete(Microsoft.EntityFrameworkCore.DeleteBehavior.Cascade);
+
+        base.Configure(builder);
+    }
+}
