7d693b9e12e71dec3bf3af39e7b1b3db18388988
Erfan <ErfanMelon@outlook.com>
2025-04-20 19:41:09 +0330

    feat: Add endpoint for inviting users to workspace

---
+++ src/Web.Api/Endpoints/Subscriptions/Invite.cs
@@ -0,0 +1,27 @@
+﻿
+using Application.Subscription.InviteUser;
+using Domain.Subscriptions;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Subscriptions;
+
+internal sealed class Invite : IEndpoint
+{
+    public sealed record Request(string Email, Guid WorkspaceId, ProjectRole Role);
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("workspace/invite", async (Request request, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new InviteUserCommand(request.Email, request.WorkspaceId, request.Role);
+
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Subscriptions);
+    }
+}
