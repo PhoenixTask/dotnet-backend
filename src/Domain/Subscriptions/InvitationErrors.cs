420f314515d937cd378d9e67c603a0b9c17973b2
Erfan <ErfanMelon@outlook.com>
2025-04-20 19:40:07 +0330

    feat: Add InvitationErrors class

---
+++ src/Domain/Subscriptions/InvitationErrors.cs
@@ -0,0 +1,16 @@
+﻿using SharedKernel;
+
+namespace Domain.Subscriptions;
+public static class InvitationErrors
+{
+    public static readonly Error RequestSentBefore = Error.Conflict(
+         "Invitation.RequestSentBefore",
+         "The invitation already sent to user");
+
+    public static readonly Error InviteMyself = Error.Conflict(
+         "Invitation.InviteMyself",
+         "You are already access this workspaces");
+    public static Error NotFound => Error.NotFound(
+        "Invitation.NotFound",
+        $"No invitation found");
+}
