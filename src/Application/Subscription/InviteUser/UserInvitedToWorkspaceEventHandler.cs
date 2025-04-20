040fe75b983abdfeb22e73ffbee5570498f619b7
Erfan <ErfanMelon@outlook.com>
2025-04-20 19:39:58 +0330

    feat: Add UserInvitedToWorkspaceEventHandler

---
+++ src/Application/Subscription/InviteUser/UserInvitedToWorkspaceEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using Application.Abstractions.Emails;
+using Domain.Subscriptions;
+using MediatR;
+
+namespace Application.Subscription.InviteUser;
+internal sealed class UserInvitedToWorkspaceEventHandler(IEmailService emailService) : INotificationHandler<UserInvitedToWorkspaceDomainEvent>
+{
+    public Task Handle(UserInvitedToWorkspaceDomainEvent notification, CancellationToken cancellationToken)
+    {
+        if (!notification.Invitation.IsApproved)
+        {
+            emailService.SendEmailAsync(notification.Invitation.Invited.Email,
+                "Invitation Request",
+                $"You are invited to workspace {notification.Invitation.Workspace.Name} " +
+                $"To Accept and join to this workspace click below link " +
+                $"FrontEndProject{notification.Invitation.Token}");
+        }
+        return Task.CompletedTask;
+    }
+}
