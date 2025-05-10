9030deaa76667f110cef1425271a7eaa34ca5505
Erfan <ErfanMelon@outlook.com>
2025-05-10 12:16:46 +0330

    feat: get shared workspaces when fetch all workspaces

--- src/Application/Workspaces/Get/GetWorkspacesQueryHandler.cs
+++ src/Application/Workspaces/Get/GetWorkspacesQueryHandler.cs
@@ -2,6 +2,7 @@
 using Application.Abstractions.Data;
 using Application.Abstractions.Messaging;
 using Domain.Users;
+using Domain.Workspaces;
 using Microsoft.EntityFrameworkCore;
 using SharedKernel;
 
@@ -12,14 +13,19 @@ internal sealed class GetWorkspacesQueryHandler(IApplicationDbContext context, I
 {
     public async Task<Result<List<WorkspaceResponse>>> Handle(GetWorkspacesQuery request, CancellationToken cancellationToken)
     {
-        return await context.Workspaces
-            .Where(x => x.CreatedById == userContext.UserId )
-            .Select(x => new WorkspaceResponse
-            {
-                Id = x.Id,
-                Name = x.Name,
-                Color = x.Color,
-            })
-            .ToListAsync(cancellationToken);
+        IQueryable<Workspace> myWorkspaces = context.Workspaces
+            .Where(x => x.CreatedById == userContext.UserId);
+
+        IQueryable<Workspace> sharedWorkspaces = context.Members
+           .Where(x => x.UserId == userContext.UserId)
+           .Select(x=>x.Workspace);
+
+        return await myWorkspaces.Concat(sharedWorkspaces).Select(x => new WorkspaceResponse
+         {
+             Id = x.Id,
+             Name = x.Name,
+             Color = x.Color,
+         })
+         .ToListAsync(cancellationToken);
     }
 }
