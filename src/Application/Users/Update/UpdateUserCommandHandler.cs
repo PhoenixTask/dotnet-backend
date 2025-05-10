37e86d9cab5d9d8b87cb3ac621e104ba43126924
Erfan <ErfanMelon@outlook.com>
2025-05-10 11:23:13 +0330

    feat: add handler to proccess user update request

---
+++ src/Application/Users/Update/UpdateUserCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+namespace Application.Users.Update;
+
+internal sealed class UpdateUserCommandHandler(
+    IApplicationDbContext context, IUserContext userContext) : ICommandHandler<UpdateUserCommand>
+{
+    public async Task<Result> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    {
+        Guid userId = userContext.UserId;
+        User? user = await context.Users.SingleOrDefaultAsync(x => x.Id == userId, cancellationToken);
+
+        if (user is null)
+        {
+            return Result.Failure(UserErrors.NotFound(userId));
+        }
+        if (!string.IsNullOrWhiteSpace(request.FirstName))
+        {
+            user.FirstName = request.FirstName;
+        }
+        if (!string.IsNullOrWhiteSpace(request.LastName))
+        {
+            user.LastName = request.LastName;
+        }
+        await context.SaveChangesAsync(cancellationToken);
+        return Result.Success();
+    }
+}
