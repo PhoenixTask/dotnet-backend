a3f4694bbb173811fbf8370104711253b0ac08cd
Erfan <ErfanMelon@outlook.com>
2025-05-10 11:23:30 +0330

    feat: add endpoint for update user info

---
+++ src/Web.Api/Endpoints/Users/Update.cs
@@ -0,0 +1,26 @@
+﻿
+using Application.Users.Update;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Users;
+
+internal sealed class Update : IEndpoint
+{
+    public sealed record Request(string? FirstName ,string? LastName);
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("user/update", async (Request request, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new UpdateUserCommand(request.FirstName, request.LastName);
+
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Users);
+    }
+}
