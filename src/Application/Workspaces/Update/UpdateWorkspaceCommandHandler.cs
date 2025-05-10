88b3c1a749449a2f329c519683edaf9207fb4979
Erfan <ErfanMelon@outlook.com>
2025-05-10 14:22:04 +0330

    fix: check permission when update workspace

--- src/Application/Workspaces/Update/UpdateWorkspaceCommandHandler.cs
+++ src/Application/Workspaces/Update/UpdateWorkspaceCommandHandler.cs
@@ -1,23 +1,35 @@
 ﻿using Application.Abstractions.Authentication;
 using Application.Abstractions.Data;
 using Application.Abstractions.Messaging;
+using Application.Users.AccessAction;
+using Domain.Subscriptions;
+using Domain.Users;
 using Domain.Workspaces;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using SharedKernel;
 
 namespace Application.Workspaces.Update;
 
-internal sealed class UpdateWorkspaceCommandHandler(IApplicationDbContext context, IUserContext userContext)
+internal sealed class UpdateWorkspaceCommandHandler(IApplicationDbContext context, IUserContext userContext,
+    ISender sender)
     : ICommandHandler<UpdateWorkspaceCommand>
 {
     public async Task<Result> Handle(UpdateWorkspaceCommand request, CancellationToken cancellationToken)
     {
-        Domain.Workspaces.Workspace? workspace = await context.Workspaces
-            .FirstOrDefaultAsync(x => x.Id == request.Id && x.CreatedBy == new Domain.Users.User { Id = userContext.UserId }, cancellationToken);
-        
-        if(workspace is null)
+        Workspace? workspace = await context.Workspaces
+            .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+        if (workspace is null)
         {
             return Result.Failure(WorkspaceErrors.NotFound(request.Id));
+        }
+
+        UserAccessCommand accessRequest = new(userContext.UserId, workspace.Id, typeof(Workspace),ProjectRole.Manager);
+        Result accessResult = await sender.Send(accessRequest, cancellationToken);
+        if (accessResult.IsFailure)
+        {
+            return accessResult;
         }
 
         workspace.Name = request.Name.Trim();
