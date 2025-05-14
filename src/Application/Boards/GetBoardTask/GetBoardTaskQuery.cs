fc4b0cf7146cb5b08076e8b313491691e3baae5f
Erfan <ErfanMelon@outlook.com>
2025-05-14 16:33:49 +0330

    feat: Add GetBoardTaskQuery

---
+++ src/Application/Boards/GetBoardTask/GetBoardTaskQuery.cs
@@ -0,0 +1,4 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Boards.GetBoardTask;
+public sealed record GetBoardTaskQuery(Guid ProjectId,int Page, int PageSize) : IQuery<List<BoardResponse>>;
