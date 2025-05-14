08d64791e125c689c84920d9f7e68142d829f323
Erfan <ErfanMelon@outlook.com>
2025-05-14 18:44:43 +0330

    feat: Add color property to UpdateProjectRequest

--- src/Web.Api/Endpoints/Projects/Update.cs
+++ src/Web.Api/Endpoints/Projects/Update.cs
@@ -8,14 +8,15 @@ namespace Web.Api.Endpoints.Projects;
 
 internal sealed class Update : IEndpoint
 {
-    public sealed record Request(string Name);
+    public sealed record Request(string Name,string Color);
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
         app.MapPut("project/update/{Id}", async (Guid Id , Request request ,ISender sender, CancellationToken cancellationToken) =>
         {
             var command = new UpdateProjectCommand(
                 Id,
-                request.Name);
+                request.Name,
+                request.Color);
 
             Result result = await sender.Send(command, cancellationToken);
 
