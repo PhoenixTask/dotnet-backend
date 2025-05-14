95d9450f826c42b8fe84d2dd8ee73ba9c994ef0d
Erfan <ErfanMelon@outlook.com>
2025-05-14 17:54:03 +0330

    feat: Add SearchTaskByProjectQuery

---
+++ src/Application/Tasks/Search/SearchTaskByProjectQuery.cs
@@ -0,0 +1,5 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Boards.GetBoardTask;
+
+namespace Application.Tasks.Search;
+public sealed record SearchTaskByProjectQuery(Guid ProjectId, string Parameter, int Page, int PageSize) : IQuery<List<BoardResponse>>;
