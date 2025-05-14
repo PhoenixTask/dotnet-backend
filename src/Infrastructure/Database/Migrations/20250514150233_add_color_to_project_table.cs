85b18379a6c5a754ebfb10669f6d5ded72abf92e
Erfan <ErfanMelon@outlook.com>
2025-05-14 18:44:17 +0330

    feat: Add color property to Project table

---
+++ src/Infrastructure/Database/Migrations/20250514150233_add_color_to_project_table.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Database.Migrations;
+
+/// <inheritdoc />
+public partial class add_color_to_project_table : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "color",
+            schema: "public",
+            table: "projects",
+            type: "TEXT",
+            nullable: false,
+            defaultValue: "");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "color",
+            schema: "public",
+            table: "projects");
+    }
+}
