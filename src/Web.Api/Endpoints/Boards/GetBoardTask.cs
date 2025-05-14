c80fc33d26cfac4f41abe9963b86e65ec931ab5a
Erfan <ErfanMelon@outlook.com>
2025-05-14 16:34:42 +0330

    feat: Add GetBoardTask endpoint

---
+++ src/Web.Api/Endpoints/Boards/GetBoardTask.cs
@@ -0,0 +1,24 @@
+﻿using Application.Boards.GetBoardTask;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Boards;
+
+internal sealed class GetBoardTask : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("project/{projectId}/get-board-task", async (Guid projectId, int page, int pageSize, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new GetBoardTaskQuery(projectId, page, pageSize);
+
+            Result<List<BoardResponse>> result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Boards);
+    }
+}
