af137c7f79f9e3784445bc44e20e42c01729c373
Erfan <ErfanMelon@outlook.com>
2025-05-14 16:33:29 +0330

    feat: Add BoardResponse DTO

---
+++ src/Application/Boards/GetBoardTask/BoardResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Boards.GetBoardTask;
+
+//internal sealed class GetBoardTaskQuery
+public sealed class BoardResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Color { get; set; }
+    public int Order { get; set; }
+    public bool IsArchive { get; set; }
+    public List<TaskResponse> TaskResponses { get; set; }
+}
