9984a01dd8023972a4fc25c39951ac8911bbf41d
Erfan <ErfanMelon@outlook.com>
2025-05-16 20:28:28 +0330

    fix: Handle null DeadLine in CreateTaskCommandValidator

--- src/Application/Tasks/Create/CreateTaskCommandValidator.cs
+++ src/Application/Tasks/Create/CreateTaskCommandValidator.cs
@@ -10,7 +10,7 @@ internal sealed class CreateTaskCommandValidator : AbstractValidator<CreateTaskC
         RuleFor(x => x.Name).NotEmpty();
         RuleFor(x => x.BoardId).NotEmpty();
         RuleFor(x => x.Description).MaximumLength(225);
-        RuleFor(x => x.DeadLine).GreaterThanOrEqualTo(DateTime.Now).WithErrorCode(TaskErrors.ExpiredDeadLine.Code).WithMessage(TaskErrors.ExpiredDeadLine.Description);
+        RuleFor(x => x.DeadLine).GreaterThanOrEqualTo(DateTime.Now).WithErrorCode(TaskErrors.ExpiredDeadLine.Code).WithMessage(TaskErrors.ExpiredDeadLine.Description).When(x=>x.DeadLine.HasValue);
         RuleFor(x => x.Order).ExclusiveBetween(-100, 100);
         RuleFor(x => x.Priority).ExclusiveBetween(-100, 100);
     }
