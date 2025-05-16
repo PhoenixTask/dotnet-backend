56722b012b3111443d2bf2cc7ebf1f1388cf88cb
Erfan <ErfanMelon@outlook.com>
2025-05-16 20:29:08 +0330

    feat: Allow null values for task deadlines

---
+++ src/Infrastructure/Database/Migrations/20250516164401_make_deadline_optional_in_task_table.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Database.Migrations;
+
+/// <inheritdoc />
+public partial class make_deadline_optional_in_task_table : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<DateOnly>(
+            name: "dead_line",
+            schema: "public",
+            table: "tasks",
+            type: "TEXT",
+            nullable: true,
+            oldClrType: typeof(DateOnly),
+            oldType: "TEXT");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<DateOnly>(
+            name: "dead_line",
+            schema: "public",
+            table: "tasks",
+            type: "TEXT",
+            nullable: false,
+            defaultValue: new DateOnly(1, 1, 1),
+            oldClrType: typeof(DateOnly),
+            oldType: "TEXT",
+            oldNullable: true);
+    }
+}
