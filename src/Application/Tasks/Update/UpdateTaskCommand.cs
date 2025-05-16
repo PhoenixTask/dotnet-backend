d5ea25a9b2981fb9e66c2651ca042f686b72cffe
Erfan <ErfanMelon@outlook.com>
2025-05-16 20:28:49 +0330

    fix: Allow null DeadLine in UpdateTaskCommand

--- src/Application/Tasks/Update/UpdateTaskCommand.cs
+++ src/Application/Tasks/Update/UpdateTaskCommand.cs
@@ -7,7 +7,7 @@ public sealed class UpdateTaskCommand : ICommand
     public string Name { get; set; }
     public Guid BoardId { get; set; }
     public string Description { get; set; }
-    public DateTime DeadLine { get; set; }
+    public DateTime? DeadLine { get; set; }
     public int Order { get; set; }
     public int Priority { get; set; }
 }
