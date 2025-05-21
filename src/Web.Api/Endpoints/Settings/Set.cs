dd4382317ddb6c062c55081c91e5da065f2729f0
Erfan <ErfanMelon@outlook.com>
2025-05-21 13:59:56 +0330

    fix: move set setting to approprate group

--- src/Web.Api/Endpoints/Settings/Set.cs
+++ src/Web.Api/Endpoints/Settings/Set.cs
@@ -22,6 +22,6 @@ internal sealed class Set : IEndpoint
             return result.Match(Results.NoContent, CustomResults.Problem);
         })
         .RequireAuthorization()
-        .WithTags(Tags.Workspaces);
+        .WithTags(Tags.Settings);
     }
 }
