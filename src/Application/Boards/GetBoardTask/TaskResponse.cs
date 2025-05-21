682baa02020c2102100e0b5aaf9299ae2355888e
Erfan <ErfanMelon@outlook.com>
2025-05-21 13:58:07 +0330

    feat: add isComplete to task response

--- src/Application/Boards/GetBoardTask/TaskResponse.cs
+++ src/Application/Boards/GetBoardTask/TaskResponse.cs
@@ -8,4 +8,5 @@ public sealed class TaskResponse
     public string DeadLine { get; set; }
     public int Priority { get; set; }
     public int Order { get; set; }
+    public bool IsComplete { get; set; }
 }
