f2aea28c40e87a69271f7267ef1c99112f448678
Erfan <ErfanMelon@outlook.com>
2025-05-21 14:00:11 +0330

    feat: add complete task endpoint

---
+++ src/Web.Api/Endpoints/Tasks/Complete.cs
@@ -0,0 +1,24 @@
+﻿using Application.Tasks.Complete;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Tasks;
+
+internal sealed class Complete : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPatch("task/complete/{Id}", async (Guid Id,bool isComplete ,ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new CompleteTaskCommand(Id, isComplete);
+
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Tasks);
+    }
+}
