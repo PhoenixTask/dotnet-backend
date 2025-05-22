bf97468198f03f7c9cf841483d85bf35d8e2a991
Erfan <ErfanMelon@outlook.com>
2025-05-23 01:48:04 +0330

    fix: Correct board update endpoint route

--- src/Web.Api/Endpoints/Boards/Update.cs
+++ src/Web.Api/Endpoints/Boards/Update.cs
@@ -11,7 +11,7 @@ internal sealed class Update : IEndpoint
     public sealed record Request(string Name, string Color, int Order);
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapPut("board/update/{Id}", async (Guid Id , Request request ,ISender sender, CancellationToken cancellationToken) =>
+        app.MapPut("board/{Id}", async (Guid Id , Request request ,ISender sender, CancellationToken cancellationToken) =>
         {
             var command = new UpdateBoardCommand(
                Id,
