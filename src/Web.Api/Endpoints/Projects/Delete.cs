c8e916f37277941025c5868ce58ca12f930d5073
Erfan <ErfanMelon@outlook.com>
2025-05-23 01:47:56 +0330

    fix: Correct project delete endpoint route

--- src/Web.Api/Endpoints/Projects/Delete.cs
+++ src/Web.Api/Endpoints/Projects/Delete.cs
@@ -10,7 +10,7 @@ internal sealed class Delete : IEndpoint
 {
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapDelete("project/delete/{id}", async (Guid id,ISender sender, CancellationToken cancellationToken) =>
+        app.MapDelete("project/{id}", async (Guid id,ISender sender, CancellationToken cancellationToken) =>
         {
             var command = new DeleteProjectCommand(id);
 
