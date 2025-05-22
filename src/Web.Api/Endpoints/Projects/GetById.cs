90a2f86a98bffc9cd9f9f125531be576a5e101c6
Erfan <ErfanMelon@outlook.com>
2025-05-23 01:47:47 +0330

    feat: Add Get Project By Id endpoint

---
+++ src/Web.Api/Endpoints/Projects/GetById.cs
@@ -0,0 +1,25 @@
+﻿using Application.Projects.Get;
+using Application.Projects.GetById;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Projects;
+
+internal sealed class GetById : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("project/{Id}", async (Guid Id,ISender sender, CancellationToken cancellationToken) =>
+        {
+            var query = new GetProjectByIdQuery(Id);
+
+            Result<ProjectResponse> result = await sender.Send(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Projects);
+    }
+}
