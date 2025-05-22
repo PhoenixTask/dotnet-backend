929e50c1fd80cff16b6995b3149dad07e7b5be4c
Erfan <ErfanMelon@outlook.com>
2025-05-23 01:49:00 +0330

    feat: Add GetBoardByIdQuery

---
+++ src/Application/Boards/GetById/GetBoardByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Boards.Get;
+using Application.Projects.Get;
+
+namespace Application.Boards.GetById;
+public sealed record GetBoardByIdQuery(Guid BoardId) : IQuery<BoardResponse>;
