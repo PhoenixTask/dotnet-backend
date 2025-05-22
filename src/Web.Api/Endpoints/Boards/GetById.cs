4351266c47fcd6ff1fc35e0116b3b2dbb4f7a627
Erfan <ErfanMelon@outlook.com>
2025-05-23 01:48:09 +0330

    feat: Add GetById endpoint for Boards

---
+++ src/Web.Api/Endpoints/Boards/GetById.cs
@@ -0,0 +1,25 @@
+﻿using Application.Boards.Get;
+using Application.Boards.GetById;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Boards;
+
+internal sealed class GetById : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("board/{Id}", async (Guid Id, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var query = new GetBoardByIdQuery(Id);
+
+            Result<BoardResponse> result = await sender.Send(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Boards);
+    }
+}
