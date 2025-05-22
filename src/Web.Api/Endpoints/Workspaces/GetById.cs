5489f8901b28a552594c8c40edcd92f3d7d1a569
Erfan <ErfanMelon@outlook.com>
2025-05-23 01:46:31 +0330

    feat: Add GetWorkspaceById endpoint

---
+++ src/Web.Api/Endpoints/Workspaces/GetById.cs
@@ -0,0 +1,25 @@
+﻿using Application.Workspaces.Get;
+using Application.Workspaces.GetById;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Workspaces;
+
+internal sealed class GetById : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("workspace/{Id}", async (Guid Id,ISender sender, CancellationToken cancellationToken) =>
+        {
+            var query = new GetWorkspaceByIdQuery(Id);
+
+            Result<WorkspaceResponse> result = await sender.Send(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Workspaces);
+    }
+}
