728f9e6574ebb06e6344a07559f0f34b722b6a86
Erfan <ErfanMelon@outlook.com>
2025-05-23 01:46:25 +0330

    fix: Correct workspace update endpoint route

--- src/Web.Api/Endpoints/Workspaces/Update.cs
+++ src/Web.Api/Endpoints/Workspaces/Update.cs
@@ -12,7 +12,7 @@ internal sealed class Update : IEndpoint
     public sealed record Request(string Name,string Color);
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapPut("workspace/update/{Id}", async (Guid Id , Request request ,ISender sender, CancellationToken cancellationToken) =>
+        app.MapPut("workspace/{Id}", async (Guid Id , Request request ,ISender sender, CancellationToken cancellationToken) =>
         {
             var command = new UpdateWorkspaceCommand(
                 Id,
