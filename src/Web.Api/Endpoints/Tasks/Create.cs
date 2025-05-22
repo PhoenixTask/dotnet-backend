8683aa6753effd8a3fcb3efc5d20a28c8cc40d12
Erfan <ErfanMelon@outlook.com>
2025-05-23 01:47:37 +0330

    fix: Correct task creation endpoint route

--- src/Web.Api/Endpoints/Tasks/Create.cs
+++ src/Web.Api/Endpoints/Tasks/Create.cs
@@ -11,7 +11,7 @@ internal sealed class Create : IEndpoint
     public sealed record Request(string Name, Guid BoardId, string Description, DateTime? DeadLine, int Order, int Priority);
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapPost("task/create/", async (Request request, ISender sender, CancellationToken cancellationToken) =>
+        app.MapPost("task/", async (Request request, ISender sender, CancellationToken cancellationToken) =>
         {
             var command = new CreateTaskCommand
             {
