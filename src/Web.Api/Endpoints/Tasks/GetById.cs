7f5596b04a51bc2260ec177a27ac9643cf39d816
Erfan <ErfanMelon@outlook.com>
2025-05-23 01:47:21 +0330

    feat: Add Get Task By Id endpoint

---
+++ src/Web.Api/Endpoints/Tasks/GetById.cs
@@ -0,0 +1,25 @@
+﻿using Application.Tasks.Get;
+using Application.Tasks.GetById;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Tasks;
+
+internal sealed class GetById : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("task/{Id}", async (Guid boardId,Guid Id, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var query = new GetTaskByIdQuery(Id);
+
+            Result<TaskResponse> result = await sender.Send(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Tasks);
+    }
+}
