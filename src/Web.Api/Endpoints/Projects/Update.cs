5e15425f728473ce7fc6c090cbe33ddf7d2f885d
Erfan <ErfanMelon@outlook.com>
2025-05-23 01:47:42 +0330

    fix: Correct endpoint route for updating projects

--- src/Web.Api/Endpoints/Projects/Update.cs
+++ src/Web.Api/Endpoints/Projects/Update.cs
@@ -11,7 +11,7 @@ internal sealed class Update : IEndpoint
     public sealed record Request(string Name,string Color);
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapPut("project/update/{Id}", async (Guid Id , Request request ,ISender sender, CancellationToken cancellationToken) =>
+        app.MapPut("project/{Id}", async (Guid Id , Request request ,ISender sender, CancellationToken cancellationToken) =>
         {
             var command = new UpdateProjectCommand(
                 Id,
