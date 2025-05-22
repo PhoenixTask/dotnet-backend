f953f5e33bf8807b83a7ac565025c5ccc29fcde5
Erfan <ErfanMelon@outlook.com>
2025-05-23 01:48:00 +0330

    feat: Adjust project creation endpoint route

--- src/Web.Api/Endpoints/Projects/Create.cs
+++ src/Web.Api/Endpoints/Projects/Create.cs
@@ -11,7 +11,7 @@ internal sealed class Create : IEndpoint
     public sealed record Request(string Name,string Color,Guid WorkspaceId);
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapPost("project/create/", async (Request request, ISender sender, CancellationToken cancellationToken) =>
+        app.MapPost("project/", async (Request request, ISender sender, CancellationToken cancellationToken) =>
         {
             var command = new CreateProjectCommand(
                 request.Name,
