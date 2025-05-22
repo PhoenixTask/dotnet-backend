b5cc1053e61c5d9ab01bb2cdde720008e7afea01
Erfan <ErfanMelon@outlook.com>
2025-05-23 01:45:55 +0330

    feat: Add SwaggerGen configuration for API versioning

---
+++ src/Web.Api/OpenApi/ConfigureSwaggerGenOptions.cs
@@ -0,0 +1,29 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Web.Api.OpenApi;
+public class ConfigureSwaggerGenOptions : IConfigureOptions<SwaggerGenOptions>
+{
+    private readonly IApiVersionDescriptionProvider _provider;
+
+    public ConfigureSwaggerGenOptions(IApiVersionDescriptionProvider provider)
+    {
+        _provider = provider;
+    }
+
+    public void Configure(SwaggerGenOptions options)
+    {
+        foreach (ApiVersionDescription description in _provider.ApiVersionDescriptions)
+        {
+            options.SwaggerDoc(
+                description.GroupName,
+                new OpenApiInfo()
+                {
+                    Title = $"PhoenixTask.Api {description.ApiVersion}",
+                    Version = description.ApiVersion.ToString(),
+                });
+        }
+    }
+}
