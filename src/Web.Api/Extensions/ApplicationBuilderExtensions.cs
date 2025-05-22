e6ecd76fac21d2bc952706c3d37b4b98f5b437a3
Erfan <ErfanMelon@outlook.com>
2025-05-23 01:46:19 +0330

    feat: Add Swagger UI support for multiple API versions

--- src/Web.Api/Extensions/ApplicationBuilderExtensions.cs
+++ src/Web.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -1,11 +1,24 @@
-﻿namespace Web.Api.Extensions;
+﻿using System.Globalization;
+using Asp.Versioning.ApiExplorer;
+
+namespace Web.Api.Extensions;
 
 public static class ApplicationBuilderExtensions
 {
     public static IApplicationBuilder UseSwaggerWithUi(this WebApplication app)
     {
         app.UseSwagger();
-        app.UseSwaggerUI();
+        app.UseSwaggerUI(options =>
+        {
+            IReadOnlyList<ApiVersionDescription> descriptions = app.DescribeApiVersions();
+            foreach (ApiVersionDescription description in descriptions)
+            {
+                string url = $"/swagger/{description.GroupName}/swagger.json";
+                string name = description.GroupName.ToLower(CultureInfo.CurrentCulture);
+
+                options.SwaggerEndpoint(url, name);
+            }
+        });
 
         return app;
     }
