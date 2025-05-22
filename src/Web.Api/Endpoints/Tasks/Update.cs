898e7d71912b86aabe824e096d3ed363cc868f73
Erfan <ErfanMelon@outlook.com>
2025-05-23 01:47:17 +0330

    fix: Correct endpoint route for updating tasks

--- src/Web.Api/Endpoints/Tasks/Update.cs
+++ src/Web.Api/Endpoints/Tasks/Update.cs
@@ -11,7 +11,7 @@ internal sealed class Update : IEndpoint
     public sealed record Request(string Name, Guid BoardId, string Description, DateTime? DeadLine, int Order, int Priority);
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapPut("task/update/{Id}", async (Guid Id , Request request ,ISender sender, CancellationToken cancellationToken) =>
+        app.MapPut("task/{Id}", async (Guid Id , Request request ,ISender sender, CancellationToken cancellationToken) =>
         {
             var command = new UpdateTaskCommand
             {
