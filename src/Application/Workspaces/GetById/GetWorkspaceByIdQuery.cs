65e86803b9c5734e8fbd92d3a3cf29077f4f42ca
Erfan <ErfanMelon@outlook.com>
2025-05-23 01:48:29 +0330

    feat: Add GetWorkspaceByIdQuery

---
+++ src/Application/Workspaces/GetById/GetWorkspaceByIdQuery.cs
@@ -0,0 +1,5 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Workspaces.Get;
+
+namespace Application.Workspaces.GetById;
+public sealed record GetWorkspaceByIdQuery(Guid WorkspaceId) : IQuery<WorkspaceResponse>;
