49130a3ef9a1a5e9a0efcc1b3d91960da658d08e
Erfan <ErfanMelon@outlook.com>
2025-05-23 01:48:56 +0330

    feat: Add GetBoardByIdQueryHandler

---
+++ src/Application/Boards/GetById/GetBoardByIdQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Application.Boards.Get;
+using Application.Users.AccessAction;
+using Domain.Projects;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Application.Boards.GetById;
+
+internal sealed class GetBoardByIdQueryHandler(IUserContext userContext, ISender sender, IApplicationDbContext context) : IQueryHandler<GetBoardByIdQuery, BoardResponse>
+{
+    public async Task<Result<BoardResponse>> Handle(GetBoardByIdQuery request, CancellationToken cancellationToken)
+    {
+        bool boardExists = await context.Boards
+           .AnyAsync(x => x.Id == request.BoardId, cancellationToken);
+
+        if (!boardExists)
+        {
+            return Result.Failure<BoardResponse>(BoardErrors.NotFound(request.BoardId));
+        }
+
+        UserAccessCommand accessRequest = new(userContext.UserId, request.BoardId, typeof(Board));
+        Result accessResult = await sender.Send(accessRequest, cancellationToken);
+        if (accessResult.IsFailure)
+        {
+            return Result.Failure<BoardResponse>(accessResult.Error);
+        }
+
+        return await context.Boards
+            .AsNoTracking()
+            .Select(x => new BoardResponse
+            {
+                Id = x.Id,
+                Color = x.Color,
+                Name = x.Name,
+                IsArchive = x.IsArchive,
+                Order = x.Order,
+            })
+            .SingleAsync(x => x.Id == request.BoardId, cancellationToken);
+    }
+}
