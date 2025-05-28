db0efca9ddef57aa6be6fb7f46f79cb61fa0194b
Erfan <ErfanMelon@outlook.com>
2025-05-28 17:14:09 +0330

    feat: Add RefreshTokenCommand

---
+++ src/Application/Users/RefreshUserToken/RefreshTokenCommnad.cs
@@ -0,0 +1,5 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Users.Login;
+
+namespace Application.Users.RefreshUserToken;
+public sealed record RefreshTokenCommnad(Guid UserId,string RefreshToken) : ICommand<LoginResponse>;
