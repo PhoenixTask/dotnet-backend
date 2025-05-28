5d166c063a62b1b12b0b83c8a3fcda9dc1b9a1e4
Erfan <ErfanMelon@outlook.com>
2025-05-28 20:19:17 +0330

    feat: Add UploadProfileCommandValidator

---
+++ src/Application/Users/UploadProfile/UploadProfileCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using System.Buffers.Text;
+using FluentValidation;
+
+namespace Application.Users.UploadProfile;
+
+internal sealed class UploadProfileCommandValidator : AbstractValidator<UploadProfileCommand>
+{
+    public UploadProfileCommandValidator()
+    {
+        RuleFor(x => x.UserId).NotEmpty();
+
+        RuleFor(x => x.Base64File).NotEmpty().WithMessage("File is empty")
+            .Must(x=>Base64.IsValid(x)).WithMessage("File format should be base64 (remove data:image/jpeg;base64)");
+
+        RuleFor(x => x.FileName).NotEmpty()
+            .Must(x => x.Split('.').Length == 2).WithMessage("FileName should have extention defined");
+    }
+}
