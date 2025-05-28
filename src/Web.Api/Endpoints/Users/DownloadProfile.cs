d15c1b272933cbbc1a2d3b9b4e9a4e7d08b30dc8
Erfan <ErfanMelon@outlook.com>
2025-05-28 20:19:36 +0330

    feat: Add endpoint for downloading user profiles

---
+++ src/Web.Api/Endpoints/Users/DownloadProfile.cs
@@ -0,0 +1,29 @@
+﻿using Application.Users.DownloadProfile;
+using MediatR;
+using Microsoft.AspNetCore.StaticFiles;
+using SharedKernel;
+
+namespace Web.Api.Endpoints.Users;
+
+internal sealed class DownloadProfile : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("user/download", static async (Guid userId, IContentTypeProvider contentTypeProvider, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new DownloadProfileCommand(userId);
+
+            Result<string> result = await sender.Send(command, cancellationToken);
+
+            if (result.IsFailure)
+            {
+                return Results.NoContent();
+            }
+            FileStream stream = File.OpenRead(result.Value);
+            contentTypeProvider.TryGetContentType(result.Value, out string contentType);
+            return Results.File(stream, contentType, $"{userId}.{Path.GetExtension(result.Value)}");
+        })
+        //.RequireAuthorization()
+        .WithTags(Tags.Users);
+    }
+}
