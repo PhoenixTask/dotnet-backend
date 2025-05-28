702f0024f625d78512cb0848876a977fdccceefa
Erfan <ErfanMelon@outlook.com>
2025-05-28 20:19:00 +0330

    feat: Add UploadProfileCommand

---
+++ src/Application/Users/UploadProfile/UploadProfileCommand.cs
@@ -0,0 +1,9 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.UploadProfile;
+public sealed class UploadProfileCommand : ICommand
+{
+    public string Base64File { get; set; }
+    public string FileName { get; set; }
+    public Guid UserId { get; set; }
+}
