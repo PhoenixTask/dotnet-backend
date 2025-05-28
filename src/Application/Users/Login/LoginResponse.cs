95ec60c5bdb3903c995cf790ba53705e6776b417
Erfan <ErfanMelon@outlook.com>
2025-05-28 17:12:22 +0330

    feat: Create LoginResponse class

---
+++ src/Application/Users/Login/LoginResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Users.Login;
+
+public sealed class LoginResponse
+{
+    public Guid UserId { get; set; }
+    public string Token { get; set; }
+    public string RefreshToken { get; set; }
+}
