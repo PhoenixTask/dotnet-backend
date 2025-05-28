abf0562f028d427ef3f717a2c754c49b6a0fbafa
Erfan <ErfanMelon@outlook.com>
2025-05-28 17:10:18 +0330

    feat: Add RefreshToken entity

---
+++ src/Domain/Users/RefreshToken.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Users;
+public sealed class RefreshToken
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string Token { get; set; }
+    public DateTime ExpireOnUtc { get; set; }
+
+    public User User { get; set; }
+}
