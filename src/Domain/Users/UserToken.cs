0420d10d345c8813455db1b5f1d52713c27dc220
Erfan <ErfanMelon@outlook.com>
2025-05-28 17:28:55 +0330

    ref: Rename RefreshToken to UserToken

--- src/Domain/Users/UserToken.cs
+++ src/Domain/Users/UserToken.cs
@@ -1,10 +1,16 @@
 ﻿namespace Domain.Users;
-public sealed class RefreshToken
+public sealed class UserToken
 {
     public Guid Id { get; set; }
     public Guid UserId { get; set; }
     public string Token { get; set; }
+    public TokenType TokenType { get; set; }
     public DateTime ExpireOnUtc { get; set; }
 
     public User User { get; set; }
+}
+public enum TokenType
+{
+    RefreshToken = 0,
+    ForgetPasswordToken = 1
 }
