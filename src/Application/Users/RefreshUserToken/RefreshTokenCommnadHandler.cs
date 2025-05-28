8c442a5cf89115aeec314e2f256ee32dc069d049
Erfan <ErfanMelon@outlook.com>
2025-05-28 17:14:16 +0330

    feat: Add RefreshTokenCommnadHandler to refresh user tokens

---
+++ src/Application/Users/RefreshUserToken/RefreshTokenCommnadHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Application.Users.Login;
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Application.Users.RefreshUserToken;
+
+internal sealed class RefreshTokenCommnadHandler(
+    IApplicationDbContext context,
+    ITokenProvider tokenProvider) : ICommandHandler<RefreshTokenCommnad, LoginResponse>
+{
+    public async Task<Result<LoginResponse>> Handle(RefreshTokenCommnad request, CancellationToken cancellationToken)
+    {
+        RefreshToken? refreshToken = await context.RefreshTokens
+            .Include(x=>x.User)
+            .SingleOrDefaultAsync(x=>x.UserId == request.UserId && x.Token == request.RefreshToken,cancellationToken);
+        if(refreshToken is null)
+        {
+            return Result.Failure<LoginResponse>(UserErrors.InvalidPermission);
+        }
+
+        string token = tokenProvider.Create(refreshToken.User);
+        string refresh = tokenProvider.GenerateRefreshToken();
+        
+        refreshToken.Token = refresh;
+        refreshToken.ExpireOnUtc = DateTime.UtcNow.AddDays(7);
+
+        await context.SaveChangesAsync(cancellationToken);
+
+        var response = new LoginResponse { RefreshToken = refresh, Token = token, UserId = refreshToken.User.Id };
+        return response;
+    }
+}
