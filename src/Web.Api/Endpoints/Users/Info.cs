228fa091bba423241e1e54ad059c0d5de55cf5d3
Erfan <ErfanMelon@outlook.com>
2025-05-28 12:53:42 +0330

    feat: add current user info endpoint

---
+++ src/Web.Api/Endpoints/Users/Info.cs
@@ -0,0 +1,25 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Users.GetById;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Users;
+
+internal sealed class Info : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("user/info", async (IUserContext userContext, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var query = new GetUserByIdQuery(userContext.UserId);
+
+            Result<UserResponse> result = await sender.Send(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Users);
+    }
+}
