d60a7fcd799dbf7a03e1c1b9bbc5cda8163a1b70
Erfan <ErfanMelon@outlook.com>
2025-05-28 20:19:05 +0330

    feat: Add UploadProfileCommandHandler

---
+++ src/Application/Users/UploadProfile/UploadProfileCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Application.Users.UploadProfile;
+
+internal sealed class UploadProfileCommandHandler(IApplicationDbContext context) : ICommandHandler<UploadProfileCommand>
+{
+    public async Task<Result> Handle(UploadProfileCommand request, CancellationToken cancellationToken)
+    {
+        User? user = await context.Users.SingleOrDefaultAsync(x => x.Id == request.UserId, cancellationToken);
+        if (user is null)
+        {
+            return Result.Failure(UserErrors.NotFound(request.UserId));
+        }
+        string filePath = Path.Combine("uploads/user-images", $"{user.Id}{Path.GetExtension(request.FileName)}");
+
+        byte[] imageByteArray = Convert.FromBase64String(request.Base64File);
+
+        await File.WriteAllBytesAsync(filePath, imageByteArray, cancellationToken);
+
+        user.ProfileImage = filePath;
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
