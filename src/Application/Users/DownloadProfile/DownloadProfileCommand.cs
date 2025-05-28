0fe652bfe845c4ca7ccba7d026fe167b755cf5f4
Erfan <ErfanMelon@outlook.com>
2025-05-28 20:18:51 +0330

    feat: Add DownloadProfileCommand

---
+++ src/Application/Users/DownloadProfile/DownloadProfileCommand.cs
@@ -0,0 +1,5 @@
+﻿using System.Text;
+using Application.Abstractions.Messaging;
+
+namespace Application.Users.DownloadProfile;
+public sealed record DownloadProfileCommand(Guid UserId) : ICommand<string>;
