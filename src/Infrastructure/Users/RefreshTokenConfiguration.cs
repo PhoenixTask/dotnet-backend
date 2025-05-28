e712021395e617a8b84c0321728a1c5a7c042f9e
Erfan <ErfanMelon@outlook.com>
2025-05-28 17:10:45 +0330

    feat: Add RefreshTokenConfiguration

---
+++ src/Infrastructure/Users/RefreshTokenConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Users;
+internal sealed class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Token).HasMaxLength(100);
+
+        builder.HasOne(x => x.User).WithMany().HasForeignKey(x => x.UserId);
+    }
+}
