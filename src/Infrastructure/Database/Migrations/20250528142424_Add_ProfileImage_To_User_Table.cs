9b6b8cbca291cba4ed7aa19a1adcfd11af0919e4
Erfan <ErfanMelon@outlook.com>
2025-05-28 20:19:28 +0330

    feat: Add ProfileImage to User table

---
+++ src/Infrastructure/Database/Migrations/20250528142424_Add_ProfileImage_To_User_Table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Database.Migrations;
+
+/// <inheritdoc />
+public partial class Add_ProfileImage_To_User_Table : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "profile_image",
+            schema: "public",
+            table: "users",
+            type: "TEXT",
+            nullable: true);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "profile_image",
+            schema: "public",
+            table: "users");
+    }
+}
