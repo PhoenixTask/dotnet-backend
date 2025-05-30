6109fb137749aa7bc895f6bde098d8a5d8d994d1
Erfan <ErfanMelon@outlook.com>
2025-05-30 13:21:32 +0330

    feat: Add endpoint to switch task board

---
+++ src/Web.Api/Endpoints/Tasks/SwitchTaskBoard.cs
@@ -0,0 +1,27 @@
+﻿using Application.Tasks.SwitchBoard;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Tasks;
+
+internal sealed class SwitchTaskBoard : IEndpoint
+{
+    public sealed record Request(Guid BoardId, Guid TaskId);
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPatch("task/update-board", async (Request request, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new SwitchBoardCommand(
+               request.TaskId,
+               request.BoardId);
+
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+       .RequireAuthorization()
+       .WithTags(Tags.Tasks);
+    }
+}
