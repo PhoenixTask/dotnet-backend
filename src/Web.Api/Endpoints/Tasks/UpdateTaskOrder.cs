6fc58f4748025b756378b3455cc76d57f6bbd5fc
Erfan <ErfanMelon@outlook.com>
2025-05-30 13:21:45 +0330

    feat: Add endpoint for updating task order

---
+++ src/Web.Api/Endpoints/Tasks/UpdateTaskOrder.cs
@@ -0,0 +1,27 @@
+﻿using Application.Tasks.ChangeOrder;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Tasks;
+
+internal sealed class UpdateTaskOrder : IEndpoint
+{
+    public sealed record Request(Guid TaskId, int Order);
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPatch("task/update-order", async (Request request, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new ChangeTaskOrderCommand(
+               request.TaskId,
+               request.Order);
+
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+       .RequireAuthorization()
+       .WithTags(Tags.Tasks);
+    }
+}
