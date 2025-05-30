b058d7721f33bfd1329f746436e5e7e4a4b82476
Erfan <ErfanMelon@outlook.com>
2025-05-30 13:20:47 +0330

    feat: Add ChangeTaskOrderCommandHandler

---
+++ src/Application/Tasks/ChangeOrder/ChangeTaskOrderCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Domain.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+using Task = Domain.Tasks.Task;
+
+
+namespace Application.Tasks.ChangeOrder;
+
+internal sealed class ChangeTaskOrderCommandHandler
+    (IApplicationDbContext context, IUserContext userContext) : ICommandHandler<ChangeTaskOrderCommand>
+{
+    public async Task<Result> Handle(ChangeTaskOrderCommand request, CancellationToken cancellationToken)
+    {
+        Guid userId = userContext.UserId;
+        Task? task = await context.Tasks.SingleOrDefaultAsync(x => x.Id == request.TaskId && x.CreatedById == userId, cancellationToken);
+
+        if (task is null)
+        {
+            return Result.Failure(TaskErrors.NotFound(request.TaskId));
+        }
+
+        task.Order = request.Order;
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
