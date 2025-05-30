4d9ad7d691def85d7287d1c0f67aa39729671438
Erfan <ErfanMelon@outlook.com>
2025-05-30 13:20:52 +0330

    feat: Add ChangeTaskOrderCommandValidator

---
+++ src/Application/Tasks/ChangeOrder/ChangeTaskOrderCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+
+namespace Application.Tasks.ChangeOrder;
+
+internal sealed class ChangeTaskOrderCommandValidator : AbstractValidator<ChangeTaskOrderCommand>
+{
+    public ChangeTaskOrderCommandValidator()
+    {
+        RuleFor(x => x.Order).ExclusiveBetween(-100, 100);
+    }
+}
