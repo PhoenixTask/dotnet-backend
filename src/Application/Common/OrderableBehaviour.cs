d54d8a369ca0bc181c94c1665a904463e124f567
Erfan <ErfanMelon@outlook.com>
2025-06-05 22:10:25 +0330

    feat: Add OrderableBehaviour to manage entity ordering

---
+++ src/Application/Common/OrderableBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using Domain.Interfaces;
+using Domain.Projects;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Application.Common;
+public static class OrderableBehaviour
+{
+    public static int PutInOrder<T>(this IQueryable<T> query, ref T entity, int newOrder) where T : Entity, IOrderable
+    {
+        entity.Order = newOrder;
+        if (!query.Any()) // Set zero for empty list
+        {
+            return entity.Order = 0;
+        }
+
+        IQueryable<int> sortedList = query
+            .OrderBy(x => x.Order)
+            .Select(x => x.Order);
+
+        int firstOrder = sortedList.FirstOrDefault();
+        int lastOrder = sortedList.LastOrDefault();
+        if (newOrder < firstOrder)
+        {
+            entity.Order = firstOrder;
+        }
+        if (newOrder > lastOrder)
+        {
+            entity.Order = lastOrder + 1;
+        }
+
+        int actualOrder = entity.Order;
+
+        return query
+            .Where(x => x.Order >= actualOrder)
+            .ExecuteUpdate(x => x.SetProperty(x => x.Order, x => x.Order + 1));
+
+    }
+    public static int DropFromOrderedList<T>(this IQueryable<T> query, int entityOrder) where T : Entity, IOrderable
+    {
+        return query
+            .Where(x => x.Order > entityOrder)
+            .ExecuteUpdate(x => x.SetProperty(x => x.Order, x => x.Order - 1));
+    }
+}
