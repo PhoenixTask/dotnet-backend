1cf9ee9d12e9fb15a377699fc9960946d151a7e3
Erfan Memarpour <e.memarpour@outlook.com>
2025-06-07 14:22:04 +0330

    feat: handle request model binding exception

--- src/Web.Api/Infrastructure/GlobalExceptionHandler.cs
+++ src/Web.Api/Infrastructure/GlobalExceptionHandler.cs
@@ -19,6 +19,15 @@ internal sealed class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> log
             Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1",
             Title = "Server failure"
         };
+        if (exception is BadHttpRequestException)
+        {
+            problemDetails = new ProblemDetails
+            {
+                Status = StatusCodes.Status400BadRequest,
+                Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1",
+                Title = "Invalid Payload"
+            };
+        }
 
         httpContext.Response.StatusCode = problemDetails.Status.Value;
 
