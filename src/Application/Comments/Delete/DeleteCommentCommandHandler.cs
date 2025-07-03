fb34ed9e25130dd232bcbd413ec05aad46ec7018
Erfan <ErfanMelon@outlook.com>
2025-07-03 17:35:26 +0330

    feat: Handle comment deletion command

---
+++ src/Application/Comments/Delete/DeleteCommentCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Domain.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Application.Comments.Delete;
+
+internal sealed class DeleteCommentCommandHandler
+    (IApplicationDbContext context) : ICommandHandler<DeleteCommentCommand>
+{
+    public async Task<Result> Handle(DeleteCommentCommand request, CancellationToken cancellationToken)
+    {
+        Comment? comment = await context.Comments.SingleOrDefaultAsync(x => x.Id == request.CommentId, cancellationToken);
+
+        if (comment is null)
+        {
+            return Result.Failure(CommentErrors.NotFound(request.CommentId));
+        }
+
+        context.Comments.Remove(comment);
+        await context.SaveChangesAsync(cancellationToken);
+        return Result.Success();
+    }
+}
