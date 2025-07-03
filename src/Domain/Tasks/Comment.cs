26cc57660680f645157ccca13ff2a83400e3829e
Erfan <ErfanMelon@outlook.com>
2025-07-03 17:33:34 +0330

    feat: Add Comment entity to Domain.Tasks

---
+++ src/Domain/Tasks/Comment.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Interfaces;
+using Domain.Users;
+using SharedKernel;
+
+namespace Domain.Tasks;
+public sealed class Comment : Entity, IBlamableEntity, IAuditableEntity, ISoftDeletableEntity
+{
+    public Guid Id { get; set; }
+    public string Content { get; set; }
+    public Task Task { get; set; }
+    public Guid TaskId { get; set; }
+
+    public User? CreatedBy { get; }
+
+    public Guid? CreatedById { get; }
+    public User? ModifiedBy { get; }
+
+    public Guid? ModifiedById { get; }
+
+    public DateTime? CreatedOnUtc { get; }
+
+    public DateTime? ModifiedOnUtc { get; }
+
+    public DateTime? DeletedOnUtc { get; }
+
+    public bool Deleted { get; }
+
+    public User? DeletedBy { get; }
+}
