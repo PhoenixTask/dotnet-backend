d85dbaff3c402c62d6b2ae0fb7a5cc0ff9a18122
Erfan <ErfanMelon@outlook.com>
2025-07-03 17:35:41 +0330

    feat: Add GetTaskCommentsQueryHandler to retrieve task comments

---
+++ src/Application/Comments/Get/GetTaskCommentsQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Application.Comments.Get;
+
+internal sealed class GetTaskCommentsQueryHandler
+    (IApplicationDbContext context) : IQueryHandler<GetTaskCommentsQuery, List<CommentResponse>>
+{
+    public async Task<Result<List<CommentResponse>>> Handle(GetTaskCommentsQuery request, CancellationToken cancellationToken)
+    {
+        return await context.Comments.Include(x => x.CreatedBy)
+            .Where(x => x.TaskId == request.TaskId)
+            .Select(x => new CommentResponse
+            {
+                CommentId = x.Id,
+                Content = x.Content,
+                PublishedOn = x.CreatedOnUtc,
+                UserName = x.CreatedBy!.UserName,
+                FullName = $"{x.CreatedBy.FirstName} {x.CreatedBy.LastName}"
+            }).ToListAsync(cancellationToken);
+    }
+}
