8503447fd3d993c168821a611bc309ce1188ca6f
Erfan <ErfanMelon@outlook.com>
2025-07-03 17:35:33 +0330

    feat: Add CommentResponse DTO

---
+++ src/Application/Comments/Get/CommentResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Comments.Get;
+
+public sealed class CommentResponse
+{
+    public Guid CommentId { get; set; }
+    public string Content { get; set; }
+    public string UserName { get; set; }
+    public string? FullName { get; set; }
+    public DateTime? PublishedOn { get; set; }
+}
