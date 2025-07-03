2769d035c3d8ac2f2f97c3b7b4234edfa4257de8
Erfan <ErfanMelon@outlook.com>
2025-07-03 17:33:42 +0330

    feat: Add CommentErrors class

---
+++ src/Domain/Tasks/CommentErrors.cs
@@ -0,0 +1,12 @@
+﻿using SharedKernel;
+
+namespace Domain.Tasks;
+public static class CommentErrors
+{
+    public static Error NotFound(Guid commentId)
+    {
+        return Error.NotFound(
+        "Comment.NotFound",
+        $"The comment with the Id = '{commentId}' was not found");
+    }
+}
