9053b4baf73fb223cefe4fa28e866cd5d53bee26
Erfan <ErfanMelon@outlook.com>
2025-07-03 17:36:42 +0330

    feat: Add endpoint for updating comments

---
+++ src/Web.Api/Endpoints/Comments/Update.cs
@@ -0,0 +1,25 @@
+﻿using Application.Comments.Update;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Comments;
+
+internal sealed class Update : IEndpoint
+{
+    public sealed record Request(string Content);
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPut("comment/{Id}", async (Guid Id, Request request, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new UpdateCommentCommand(Id, request.Content);
+
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Comments);
+    }
+}
