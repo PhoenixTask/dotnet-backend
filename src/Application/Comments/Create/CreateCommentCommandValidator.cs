f5f3f6275e082d58fb11844fd31e00d1403367b1
Erfan <ErfanMelon@outlook.com>
2025-07-03 17:35:05 +0330

    feat: Add CreateCommentCommandValidator

---
+++ src/Application/Comments/Create/CreateCommentCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Application.Comments.Create;
+
+internal sealed class CreateCommentCommandValidator : AbstractValidator<CreateCommentCommand>
+{
+    public CreateCommentCommandValidator()
+    {
+        RuleFor(x => x.Content).NotEmpty();
+    }
+}
