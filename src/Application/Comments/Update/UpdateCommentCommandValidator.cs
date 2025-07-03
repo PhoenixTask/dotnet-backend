4da4ab13e645f9936717f9f8d1b2bc99bf8a10d8
Erfan <ErfanMelon@outlook.com>
2025-07-03 17:36:00 +0330

    feat: Add UpdateCommentCommandValidator

---
+++ src/Application/Comments/Update/UpdateCommentCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Application.Comments.Update;
+
+internal sealed class UpdateCommentCommandValidator : AbstractValidator<UpdateCommentCommand>
+{
+    public UpdateCommentCommandValidator()
+    {
+        RuleFor(x => x.Content).NotEmpty();
+    }
+}
