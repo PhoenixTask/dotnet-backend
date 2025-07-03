b0f100ebd2abd4ae0c74dd39f8255f2cfeb5ef5d
Erfan <ErfanMelon@outlook.com>
2025-07-03 17:36:33 +0330

    feat: Add endpoint for deleting comments

---
+++ src/Web.Api/Endpoints/Comments/Delete.cs
@@ -0,0 +1,24 @@
+﻿using Application.Comments.Delete;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Comments;
+
+internal sealed class Delete : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("comment/{id}", async (Guid id, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new DeleteCommentCommand(id);
+
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Comments);
+    }
+}
