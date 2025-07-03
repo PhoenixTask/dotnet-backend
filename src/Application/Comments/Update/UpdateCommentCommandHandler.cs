2dcf00cd19a94a6dc1f93689c62cc994be2c035b
Erfan <ErfanMelon@outlook.com>
2025-07-03 17:35:55 +0330

    feat: Add UpdateCommentCommandHandler

---
+++ src/Application/Comments/Update/UpdateCommentCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Domain.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Application.Comments.Update;
+
+internal sealed class UpdateCommentCommandHandler
+    (IApplicationDbContext context) : ICommandHandler<UpdateCommentCommand>
+{
+    public async Task<Result> Handle(UpdateCommentCommand request, CancellationToken cancellationToken)
+    {
+        Comment? comment = await context.Comments.SingleOrDefaultAsync(x => x.Id == request.CommentId, cancellationToken);
+        if (comment is null)
+        {
+            return Result.Failure(CommentErrors.NotFound(request.CommentId));
+        }
+
+        comment.Content = request.Content.Trim();
+        await context.SaveChangesAsync(cancellationToken);
+        return Result.Success();
+    }
+}
