ab75ea4efe81c4b7d038a9fc97c7a0ec47bd5133
Erfan <ErfanMelon@outlook.com>
2025-07-03 17:36:06 +0330

    feat: Add CommentConfiguration class

---
+++ src/Infrastructure/Tasks/CommentConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Tasks;
+
+internal sealed class CommentConfiguration : CommonEntityConfiguration<Comment>
+{
+    public override void Configure(EntityTypeBuilder<Comment> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.HasOne(x => x.Task)
+            .WithMany()
+            .HasForeignKey(x => x.TaskId);
+
+        base.Configure(builder);
+    }
+}
