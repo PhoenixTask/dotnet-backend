18832a2e851910be4d36ef1871a4533f9e2eedbe
Erfan <ErfanMelon@outlook.com>
2025-07-11 15:17:10 +0330

    feat: Add validation for ChangeBoardsOrderCommand

---
+++ src/Application/Boards/ChangeOrder/ChangeBoardsOrderCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Application.Boards.ChangeOrder;
+
+internal sealed class ChangeBoardsOrderCommandValidator : AbstractValidator<ChangeBoardsOrderCommand>
+{
+    public ChangeBoardsOrderCommandValidator()
+    {
+        RuleFor(x => x.Boards)
+             .NotEmpty();
+
+        RuleForEach(x => x.Boards)
+            .ChildRules(board =>
+            {
+                board.RuleFor(b => b.Order)
+                     .GreaterThanOrEqualTo(1);
+
+                board.RuleFor(b => b.Id)
+                     .NotEmpty();
+            });
+    }
+}
