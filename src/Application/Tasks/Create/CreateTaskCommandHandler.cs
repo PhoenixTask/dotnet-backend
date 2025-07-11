42a061da3a27681a9cee690d6b8a5b4564549396
Erfan <ErfanMelon@outlook.com>
2025-07-11 15:17:54 +0330

    ref: Remove unused using statement

--- src/Application/Tasks/Create/CreateTaskCommandHandler.cs
+++ src/Application/Tasks/Create/CreateTaskCommandHandler.cs
@@ -1,7 +1,6 @@
 ﻿using Application.Abstractions.Authentication;
 using Application.Abstractions.Data;
 using Application.Abstractions.Messaging;
-using Application.Common;
 using Domain.Projects;
 using Microsoft.EntityFrameworkCore;
 using SharedKernel;
@@ -10,20 +9,20 @@ using Task = Domain.Tasks.Task;
 namespace Application.Tasks.Create;
 
 internal sealed class CreateTaskCommandHandler(
-    IApplicationDbContext context , IUserContext userContext) : ICommandHandler<CreateTaskCommand, Guid>
+    IApplicationDbContext context, IUserContext userContext) : ICommandHandler<CreateTaskCommand, Guid>
 {
     public async Task<Result<Guid>> Handle(CreateTaskCommand request, CancellationToken cancellationToken)
     {
         Guid userId = userContext.UserId;
         Board? board = await context.Boards
             .SingleOrDefaultAsync(x => x.Id == request.BoardId && x.CreatedById == userId, cancellationToken: cancellationToken);
-        
-        if(board is null)
+
+        if (board is null)
         {
             return Result.Failure<Guid>(BoardErrors.NotFound(request.BoardId));
         }
 
-        DateOnly? deadLine =DateOnly.FromDateTime(request.DeadLine.GetValueOrDefault());
+        DateOnly? deadLine = DateOnly.FromDateTime(request.DeadLine.GetValueOrDefault());
 
         var task = new Task
         {
@@ -34,10 +33,6 @@ internal sealed class CreateTaskCommandHandler(
             Order = request.Order,
             Priority = request.Priority,
         };
-
-        context.Tasks
-          .Where(x => x.BoardId == request.BoardId)
-          .PutInOrder(ref task, request.Order);
 
         context.Tasks.Add(task);
         await context.SaveChangesAsync(cancellationToken);
