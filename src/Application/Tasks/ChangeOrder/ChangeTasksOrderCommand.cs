1d253dfe937d20a2e41e7bcb0b1e7a83bfd4a7f0
Erfan <ErfanMelon@outlook.com>
2025-07-11 15:17:41 +0330

    feat: Add ChangeTasksOrderCommand

---
+++ src/Application/Tasks/ChangeOrder/ChangeTasksOrderCommand.cs
@@ -0,0 +1,5 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Boards.ChangeOrder;
+
+namespace Application.Tasks.ChangeOrder;
+public sealed record ChangeTasksOrderCommand(List<OrderableModel> Tasks) : ICommand;
