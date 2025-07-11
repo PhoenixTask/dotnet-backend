e1b6f10f3d0f19a53faf3bf5b62f9813d63b6898
Erfan <ErfanMelon@outlook.com>
2025-07-11 15:18:07 +0330

    feat: Add API endpoint for updating board order (v2)

--- src/Web.Api/Endpoints/Boards/UpdateBoardOrder.cs
+++ src/Web.Api/Endpoints/Boards/UpdateBoardOrder.cs
@@ -5,7 +5,6 @@ using Web.Api.Extensions;
 using Web.Api.Infrastructure;
 
 namespace Web.Api.Endpoints.Boards;
-
 internal sealed class UpdateBoardOrder : IEndpoint
 {
     public sealed record Request(Guid BoardId, int Order);
@@ -22,6 +21,17 @@ internal sealed class UpdateBoardOrder : IEndpoint
             return result.Match(Results.NoContent, CustomResults.Problem);
         })
        .RequireAuthorization()
+       .AdvertisesApiVersion(2)
+       .WithTags(Tags.Boards);
+
+        app.MapPatch("board/update-order", async (ChangeBoardsOrderCommand command, ISender sender, CancellationToken cancellationToken) =>
+        {
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+       .RequireAuthorization()
+       .HasApiVersion(2)
        .WithTags(Tags.Boards);
     }
 }
