0e8f997f8c3ee5fb2ebb02746273f3d10794bf89
Erfan <ErfanMelon@outlook.com>
2025-07-11 15:17:49 +0330

    feat: Add ChangeTasksOrderCommandValidator

---
+++ src/Application/Tasks/ChangeOrder/ChangeTasksOrderCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Application.Tasks.ChangeOrder;
+
+internal sealed class ChangeTasksOrderCommandValidator : AbstractValidator<ChangeTasksOrderCommand>
+{
+    public ChangeTasksOrderCommandValidator()
+    {
+        RuleFor(x => x.Tasks)
+             .NotEmpty();
+
+        RuleForEach(x => x.Tasks)
+            .ChildRules(task =>
+            {
+                task.RuleFor(t => t.Order)
+                     .GreaterThanOrEqualTo(1);
+
+                task.RuleFor(t => t.Id)
+                     .NotEmpty();
+            });
+    }
+}
