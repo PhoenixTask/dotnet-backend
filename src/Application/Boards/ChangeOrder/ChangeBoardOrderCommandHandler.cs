ceee17eff77c7a177f825792a030af130ea24c84
Erfan <ErfanMelon@outlook.com>
2025-07-17 19:46:28 +0330

    fix: reorder board in project

--- src/Application/Boards/ChangeOrder/ChangeBoardOrderCommandHandler.cs
+++ src/Application/Boards/ChangeOrder/ChangeBoardOrderCommandHandler.cs
@@ -12,14 +12,29 @@ internal sealed class ChangeBoardOrderCommandHandler(IApplicationDbContext conte
     public async Task<Result> Handle(ChangeBoardOrderCommand request, CancellationToken cancellationToken)
     {
         Guid userId = userContext.UserId;
-        Board? board = await context.Boards.SingleOrDefaultAsync(x => x.Id == request.BoardId && x.CreatedById == userId, cancellationToken);
+        Board? board = await context.Boards
+            .SingleOrDefaultAsync(x => x.Id == request.BoardId && x.CreatedById == userId, cancellationToken);
 
         if (board is null)
         {
             return Result.Failure(BoardErrors.NotFound(request.BoardId));
         }
 
+
+        List<Board> boards = await context.Boards
+            .Where(x => x.ProjectId == board.ProjectId && x.Id != board.Id)
+            .OrderBy(x => x.Order)
+            .ToListAsync(cancellationToken);
+
+        for (int i = 0; i < boards.Count; i++)
+        {
+            boards[i].Order = i + 1;
+        }
         board.Order = request.Order;
+        foreach (Board? b in boards.Where(x => x.Order >= request.Order))
+        {
+            b.Order++;
+        }
 
         await context.SaveChangesAsync(cancellationToken);
 
