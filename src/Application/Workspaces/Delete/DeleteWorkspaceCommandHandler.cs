33564c9f583653908ee7c203ea1f544b16eb50c8
Erfan <ErfanMelon@outlook.com>
2025-07-17 18:17:05 +0330

    fix: remove project board and task on workspace delete

--- src/Application/Workspaces/Delete/DeleteWorkspaceCommandHandler.cs
+++ src/Application/Workspaces/Delete/DeleteWorkspaceCommandHandler.cs
@@ -7,19 +7,22 @@ using SharedKernel;
 
 namespace Application.Workspaces.Delete;
 
-internal sealed class DeleteWorkspaceCommandHandler(IApplicationDbContext context,IUserContext userContext)
+internal sealed class DeleteWorkspaceCommandHandler(IApplicationDbContext context, IUserContext userContext)
     : ICommandHandler<DeleteWorkspaceCommand>
 {
     public async Task<Result> Handle(DeleteWorkspaceCommand request, CancellationToken cancellationToken)
     {
-        Workspace? workspace =await context.Workspaces
-            .SingleOrDefaultAsync(x =>x.Id == request.Id && x.CreatedById == userContext.UserId, cancellationToken: cancellationToken);
-        
+        Workspace? workspace = await context.Workspaces
+            .Include(x => x.Projects)
+            .ThenInclude(x => x.Boards)
+            .ThenInclude(x => x.Tasks)
+            .SingleOrDefaultAsync(x => x.Id == request.Id && x.CreatedById == userContext.UserId,
+            cancellationToken);
+
         if (workspace is null)
         {
             return Result.Failure(WorkspaceErrors.NotFound(request.Id));
         }
-
         context.Workspaces.Remove(workspace);
         int rowAffect = await context.SaveChangesAsync(cancellationToken);
 
