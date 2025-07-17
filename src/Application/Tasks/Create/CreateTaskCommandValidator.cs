9cbd453a895f5aacb733668009f49516caebcc77
Erfan <ErfanMelon@outlook.com>
2025-07-17 18:39:35 +0330

    fix: Correct validation rule for task order

--- src/Application/Tasks/Create/CreateTaskCommandValidator.cs
+++ src/Application/Tasks/Create/CreateTaskCommandValidator.cs
@@ -10,8 +10,7 @@ internal sealed class CreateTaskCommandValidator : AbstractValidator<CreateTaskC
         RuleFor(x => x.Name).NotEmpty();
         RuleFor(x => x.BoardId).NotEmpty();
         RuleFor(x => x.Description).MaximumLength(225);
-        RuleFor(x => x.DeadLine).GreaterThanOrEqualTo(DateTime.Now).WithErrorCode(TaskErrors.ExpiredDeadLine.Code).WithMessage(TaskErrors.ExpiredDeadLine.Description).When(x=>x.DeadLine.HasValue);
-        RuleFor(x => x.Order).ExclusiveBetween(int.MinValue, int.MaxValue);
+        RuleFor(x => x.DeadLine).GreaterThanOrEqualTo(DateTime.Now).WithErrorCode(TaskErrors.ExpiredDeadLine.Code).WithMessage(TaskErrors.ExpiredDeadLine.Description).When(x => x.DeadLine.HasValue);
         RuleFor(x => x.Priority).ExclusiveBetween(-100, 100);
     }
 }
