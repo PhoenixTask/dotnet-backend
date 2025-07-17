6d09d50042ce3a7dc8f61aec9552e4bdc2ddc6fb
Erfan <ErfanMelon@outlook.com>
2025-07-17 18:45:29 +0330

    feat: Remove Order property from UpdateBoardCommand

--- src/Application/Boards/Update/UpdateBoardCommand.cs
+++ src/Application/Boards/Update/UpdateBoardCommand.cs
@@ -1,4 +1,4 @@
 ﻿using Application.Abstractions.Messaging;
 
 namespace Application.Boards.Update;
-public sealed record UpdateBoardCommand(Guid BoardId, string Name, string Color, int Order) : ICommand;
+public sealed record UpdateBoardCommand(Guid BoardId, string Name, string Color) : ICommand;
