81eeb780ed352de591168945a8c967ea16763551
Erfan <ErfanMelon@outlook.com>
2025-07-17 18:45:46 +0330

    feat: Remove BoardId and Order from UpdateTaskCommand

--- src/Application/Tasks/Update/UpdateTaskCommand.cs
+++ src/Application/Tasks/Update/UpdateTaskCommand.cs
@@ -5,9 +5,7 @@ public sealed class UpdateTaskCommand : ICommand
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
-    public Guid BoardId { get; set; }
     public string Description { get; set; }
     public DateTime? DeadLine { get; set; }
-    public int Order { get; set; }
     public int Priority { get; set; }
 }
