823d1581e00fe67ba8cdbe2b228da7acfbe64a73
Erfan <ErfanMelon@outlook.com>
2025-07-17 18:38:45 +0330

    feat: Remove Order property from CreateBoardCommand

--- src/Application/Boards/Create/CreateBoardCommand.cs
+++ src/Application/Boards/Create/CreateBoardCommand.cs
@@ -1,4 +1,4 @@
 ﻿using Application.Abstractions.Messaging;
 
 namespace Application.Boards.Create;
-public sealed record CreateBoardCommand(Guid ProjectId, string Name, string Color, int Order) : ICommand<Guid>;
+public sealed record CreateBoardCommand(Guid ProjectId, string Name, string Color) : ICommand<Guid>;
