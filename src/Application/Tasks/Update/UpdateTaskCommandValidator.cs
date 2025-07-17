75ec78ae74fa21d7a0abe7e6c5c61ad8d54c0687
Erfan <ErfanMelon@outlook.com>
2025-07-17 18:45:59 +0330

    ref: Remove unnecessary validation rules in UpdateTaskCommandValidator

--- src/Application/Tasks/Update/UpdateTaskCommandValidator.cs
+++ src/Application/Tasks/Update/UpdateTaskCommandValidator.cs
@@ -8,10 +8,8 @@ internal sealed class UpdateTaskCommandValidator : AbstractValidator<UpdateTaskC
     public UpdateTaskCommandValidator()
     {
         RuleFor(x => x.Name).NotEmpty();
-        RuleFor(x => x.BoardId).NotEmpty();
         RuleFor(x => x.Description).MaximumLength(225);
-        RuleFor(x => x.DeadLine).GreaterThanOrEqualTo(DateTime.Now).WithErrorCode(TaskErrors.ExpiredDeadLine.Code).WithMessage(TaskErrors.ExpiredDeadLine.Description).When(x=>x.DeadLine.HasValue);
-        RuleFor(x => x.Order).ExclusiveBetween(int.MinValue, int.MaxValue);
+        RuleFor(x => x.DeadLine).GreaterThanOrEqualTo(DateTime.Now).WithErrorCode(TaskErrors.ExpiredDeadLine.Code).WithMessage(TaskErrors.ExpiredDeadLine.Description).When(x => x.DeadLine.HasValue);
         RuleFor(x => x.Priority).ExclusiveBetween(-100, 100);
     }
 }
