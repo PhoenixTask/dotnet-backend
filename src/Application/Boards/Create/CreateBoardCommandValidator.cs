fcd258a32c8b5cca3c7e993bfb1a6b7572a26dd4
Erfan <ErfanMelon@outlook.com>
2025-07-17 18:38:57 +0330

    fix: Remove unnecessary order validation in CreateBoardCommandValidator

--- src/Application/Boards/Create/CreateBoardCommandValidator.cs
+++ src/Application/Boards/Create/CreateBoardCommandValidator.cs
@@ -8,6 +8,5 @@ internal sealed class CreateBoardCommandValidator : AbstractValidator<CreateBoar
     {
         RuleFor(x => x.Name).NotEmpty();
         RuleFor(x => x.Color).NotEmpty();
-        RuleFor(x => x.Order).ExclusiveBetween(int.MinValue, int.MaxValue);
     }
 }
