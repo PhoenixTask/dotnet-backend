bb968ccad1dfc3385c06571cd29da9f9e35fe610
Erfan <ErfanMelon@outlook.com>
2025-07-17 18:45:18 +0330

    feat: Remove BoardId and Order from UpdateTaskRequest

--- src/Web.Api/Endpoints/Tasks/Update.cs
+++ src/Web.Api/Endpoints/Tasks/Update.cs
@@ -8,16 +8,14 @@ namespace Web.Api.Endpoints.Tasks;
 
 internal sealed class Update : IEndpoint
 {
-    public sealed record Request(string Name, Guid BoardId, string Description, DateTime? DeadLine, int Order, int Priority);
+    public sealed record Request(string Name, string Description, DateTime? DeadLine, int Priority);
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapPut("task/{Id}", async (Guid Id , Request request ,ISender sender, CancellationToken cancellationToken) =>
+        app.MapPut("task/{Id}", async (Guid Id, Request request, ISender sender, CancellationToken cancellationToken) =>
         {
             var command = new UpdateTaskCommand
             {
-                BoardId = request.BoardId,
                 DeadLine = request.DeadLine,
-                Order = request.Order,  
                 Priority = request.Priority,
                 Description = request.Description,
                 Id = Id,
