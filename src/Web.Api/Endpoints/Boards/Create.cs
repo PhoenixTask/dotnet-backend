8e43c90edeb4a710a08ffaa59ecfa8c1ac22540d
Erfan <ErfanMelon@outlook.com>
2025-08-01 15:22:57 +0330

    feat: Add name and summary to Create Board endpoint

--- src/Web.Api/Endpoints/Boards/Create.cs
+++ src/Web.Api/Endpoints/Boards/Create.cs
@@ -22,6 +22,8 @@ internal sealed class Create : IEndpoint
 
             return result.Match(Results.Ok, CustomResults.Problem);
         })
+        .WithName("Create Board")
+        .WithSummary("Create new board")
         .RequireAuthorization()
         .WithTags(Tags.Boards);
     }
