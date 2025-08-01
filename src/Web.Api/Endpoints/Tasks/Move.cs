b5c726879c190c29480acdd25938fa93e13f1deb
Erfan <ErfanMelon@outlook.com>
2025-08-01 12:25:56 +0330

    feat: Add endpoint for moving tasks

---
+++ src/Web.Api/Endpoints/Tasks/Move.cs
@@ -0,0 +1,22 @@
+﻿using Application.Tasks.ChangeOrderBoard;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Tasks;
+
+public sealed class Move : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPatch("task/move", async (ChangeTaskBoardOrderCommand command, ISender sender, CancellationToken cancellationToken) =>
+        {
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+       .RequireAuthorization()
+       .WithTags(Tags.Tasks);
+    }
+}
