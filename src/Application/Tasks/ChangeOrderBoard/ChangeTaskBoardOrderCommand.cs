abf350802691d4a6bc0ddd14f8c0ab58f61221f7
Erfan <ErfanMelon@outlook.com>
2025-08-01 12:25:45 +0330

    feat: Add ChangeTaskBoardOrderCommand

---
+++ src/Application/Tasks/ChangeOrderBoard/ChangeTaskBoardOrderCommand.cs
@@ -0,0 +1,4 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Tasks.ChangeOrderBoard;
+public sealed record ChangeTaskBoardOrderCommand(List<TaskRequest> TaskRequests) : ICommand;
