9ce7fea085eec430625051ebf733a3df0bdf38f8
Erfan <ErfanMelon@outlook.com>
2025-08-01 12:58:49 +0330

    feat: Add validator for UpdateDeadLineCommand

---
+++ src/Application/Tasks/UpdateDeadLine/UpdateDeadLineCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Tasks;
+using FluentValidation;
+
+namespace Application.Tasks.UpdateDeadLine;
+
+internal sealed class UpdateDeadLineCommandValidator : AbstractValidator<UpdateDeadLineCommand>
+{
+    public UpdateDeadLineCommandValidator()
+    {
+        RuleFor(x => x.NewDeadLine).GreaterThanOrEqualTo(DateTime.Now).WithErrorCode(TaskErrors.ExpiredDeadLine.Code).WithMessage(TaskErrors.ExpiredDeadLine.Description).When(x => x.NewDeadLine.HasValue);
+    }
+}
