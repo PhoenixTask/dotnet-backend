f8078eac9b4e82bab3028109aaebc4f7110b2293
Erfan <ErfanMelon@outlook.com>
2025-08-01 12:58:44 +0330

    feat: Add UpdateDeadLineCommandHandler

---
+++ src/Application/Tasks/UpdateDeadLine/UpdateDeadLineCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Domain.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Application.Tasks.UpdateDeadLine;
+
+internal sealed class UpdateDeadLineCommandHandler
+    (IApplicationDbContext context) : ICommandHandler<UpdateDeadLineCommand>
+{
+    public async Task<Result> Handle(UpdateDeadLineCommand request, CancellationToken cancellationToken)
+    {
+        Domain.Tasks.Task? task = await context.Tasks.SingleOrDefaultAsync(x => x.Id == request.TaskId, cancellationToken);
+        if (task is null)
+        {
+            return Result.Failure(TaskErrors.NotFound(request.TaskId));
+        }
+
+        var deadLine = DateOnly.FromDateTime(request.NewDeadLine.GetValueOrDefault());
+
+        task.DeadLine = request.NewDeadLine.HasValue ? deadLine : null;
+
+        await context.SaveChangesAsync(cancellationToken);
+        return Result.Success();
+    }
+}
