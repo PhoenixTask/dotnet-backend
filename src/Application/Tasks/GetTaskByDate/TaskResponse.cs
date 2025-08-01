284967687c648aab5474fe93bf2cbba8cf62bdea
Erfan <ErfanMelon@outlook.com>
2025-08-01 14:01:41 +0330

    feat: Add TaskResponse DTO for GetTaskByDate

---
+++ src/Application/Tasks/GetTaskByDate/TaskResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Tasks.GetTaskByDate;
+
+public class TaskResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string DeadLine { get; set; }
+    public int Priority { get; set; }
+    public int Order { get; set; }
+    public bool IsComplete { get; set; }
+
+    public Guid BoardId { get; set; }
+    public string BoardName { get; set; }
+}
