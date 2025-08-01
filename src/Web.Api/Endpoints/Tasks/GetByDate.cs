d30a4d54be6b9aa58070668e692a52278a140c6d
Erfan <ErfanMelon@outlook.com>
2025-08-01 14:01:46 +0330

    feat: Add endpoint to retrieve tasks by date range

---
+++ src/Web.Api/Endpoints/Tasks/GetByDate.cs
@@ -0,0 +1,25 @@
+﻿using Application.Tasks.GetTaskByDate;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Tasks;
+
+internal sealed class GetByDate : IEndpoint
+{
+
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("task/get-by-deadline", async (Guid ProjectId, DateTime Start, DateTime End, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var query = new GetTaskByDeadLineQuery(ProjectId, Start, End);
+
+            Result<List<TaskResponse>> result = await sender.Send(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Tasks);
+    }
+}
