b04761dbce2854aacc80c08b9e5c217f412ada9c
Erfan <ErfanMelon@outlook.com>
2025-08-01 12:58:54 +0330

    feat: Add endpoint to update task deadline

---
+++ src/Web.Api/Endpoints/Tasks/UpdateDeadLine.cs
@@ -0,0 +1,25 @@
+﻿using Application.Tasks.UpdateDeadLine;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Tasks;
+
+internal sealed class UpdateDeadLine : IEndpoint
+{
+    public sealed record Request(Guid TaskId, DateTime? NewDeadLine);
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPatch("task/update-deadline", async (Request request, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new UpdateDeadLineCommand(request.TaskId, request.NewDeadLine);
+
+            Result result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+        .RequireAuthorization()
+        .WithTags(Tags.Tasks);
+    }
+}
