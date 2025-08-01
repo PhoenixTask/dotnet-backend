6580f4e8faec428c59afb4eeb56439237ab7810c
Erfan <ErfanMelon@outlook.com>
2025-08-01 14:01:36 +0330

    feat: Add GetTaskByDeadLineQueryValidator

---
+++ src/Application/Tasks/GetTaskByDate/GetTaskByDeadLineQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Tasks.GetTaskByDate;
+
+internal sealed class GetTaskByDeadLineQueryValidator : AbstractValidator<GetTaskByDeadLineQuery>
+{
+    public GetTaskByDeadLineQueryValidator()
+    {
+        RuleFor(x => x.StartDate).NotEmpty();
+        RuleFor(x => x.EndDate).NotEmpty();
+        RuleFor(x => x.StartDate).LessThan(x => x.EndDate);
+    }
+}
