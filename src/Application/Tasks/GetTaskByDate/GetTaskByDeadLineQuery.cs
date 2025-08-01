72da11eb4ac2ea408347554e51296f04fb8f10fd
Erfan <ErfanMelon@outlook.com>
2025-08-01 14:01:26 +0330

    feat: Add GetTaskByDeadLineQuery

---
+++ src/Application/Tasks/GetTaskByDate/GetTaskByDeadLineQuery.cs
@@ -0,0 +1,4 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Tasks.GetTaskByDate;
+public sealed record GetTaskByDeadLineQuery(Guid ProjectId, DateTime StartDate, DateTime EndDate) : IQuery<List<TaskResponse>>;
