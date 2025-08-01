546e15e0fa2a1e35fb03c2ce0b059f22a8d6e21b
Erfan <ErfanMelon@outlook.com>
2025-08-01 15:25:09 +0330

    chore: Improve readability of Create Comment endpoint

--- src/Web.Api/Endpoints/Comments/Create.cs
+++ src/Web.Api/Endpoints/Comments/Create.cs
@@ -8,17 +8,20 @@ namespace Web.Api.Endpoints.Comments;
 
 internal sealed class Create : IEndpoint
 {
-    public sealed record Request(Guid TaskId, string Comment);
+    public sealed record Request(Guid TaskId, string Content);
+
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
         app.MapPost("comment/", async (Request request, ISender sender, CancellationToken cancellationToken) =>
         {
-            var command = new CreateCommentCommand(request.TaskId, request.Comment);
+            var command = new CreateCommentCommand(request.TaskId, request.Content);
 
             Result<Guid> result = await sender.Send(command, cancellationToken);
 
             return result.Match(Results.Ok, CustomResults.Problem);
         })
+        .WithName("Create Comment")
+        .WithSummary("Create comment for a task")
         .RequireAuthorization()
         .WithTags(Tags.Comments);
     }
