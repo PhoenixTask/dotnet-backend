61ac3e76df7ecb27de017cfa5ab5f5f2b4b4a181
Erfan <ErfanMelon@outlook.com>
2025-08-06 21:29:50 +0330

    feat: add endpoint to get all tasks with board

---
+++ src/Web.Api/Endpoints/Tasks/GetTasksWithBoard.cs
@@ -0,0 +1,27 @@
+﻿using Application.Tasks.GetTaskByDate;
+using Application.Tasks.GetTaskWithBoards;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Tasks;
+
+internal sealed class GetTasksWithBoard : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("task/tasks-with-board", async (Guid ProjectId, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var query = new GetTasksWithBoardQuery(ProjectId);
+
+            Result<List<TaskResponse>> result = await sender.Send(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .WithName("Get Tasks With Board")
+        .WithSummary("Get tasks it's boards name and id for a project")
+        .RequireAuthorization()
+        .WithTags(Tags.Tasks);
+    }
+}
