8c45744869a5af56ba62fb067d6c407d2ba88527
Erfan <ErfanMelon@outlook.com>
2025-08-06 21:29:16 +0330

    feat: create a request to get tasks with it's board name and id

---
+++ src/Application/Tasks/GetTaskWithBoards/GetTasksWithBoardQuery.cs
@@ -0,0 +1,5 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Tasks.GetTaskByDate;
+
+namespace Application.Tasks.GetTaskWithBoards;
+public sealed record GetTasksWithBoardQuery(Guid ProjectId) : IQuery<List<TaskResponse>>;
