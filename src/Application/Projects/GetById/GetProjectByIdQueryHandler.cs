a694870e3fd78d593b5a182ea7549e28e64da80c
Erfan <ErfanMelon@outlook.com>
2025-08-12 22:28:13 +0330

    fix: remove access check from get project

--- src/Application/Projects/GetById/GetProjectByIdQueryHandler.cs
+++ src/Application/Projects/GetById/GetProjectByIdQueryHandler.cs
@@ -1,16 +1,13 @@
-﻿using Application.Abstractions.Authentication;
-using Application.Abstractions.Data;
+﻿using Application.Abstractions.Data;
 using Application.Abstractions.Messaging;
 using Application.Projects.Get;
-using Application.Users.AccessAction;
 using Domain.Projects;
-using MediatR;
 using Microsoft.EntityFrameworkCore;
 using SharedKernel;
 
 namespace Application.Projects.GetById;
 
-internal sealed class GetProjectByIdQueryHandler(IUserContext userContext, ISender sender, IApplicationDbContext context) : IQueryHandler<GetProjectByIdQuery, ProjectResponse>
+internal sealed class GetProjectByIdQueryHandler(IApplicationDbContext context) : IQueryHandler<GetProjectByIdQuery, ProjectResponse>
 {
     public async Task<Result<ProjectResponse>> Handle(GetProjectByIdQuery request, CancellationToken cancellationToken)
     {
@@ -20,13 +17,6 @@ internal sealed class GetProjectByIdQueryHandler(IUserContext userContext, ISend
         if (!projectExists)
         {
             return Result.Failure<ProjectResponse>(ProjectErrors.NotFound(request.ProjectId));
-        }
-
-        UserAccessCommand accessRequest = new(userContext.UserId, request.ProjectId, typeof(Project));
-        Result accessResult = await sender.Send(accessRequest, cancellationToken);
-        if (accessResult.IsFailure)
-        {
-            return Result.Failure<ProjectResponse>(accessResult.Error);
         }
 
         return await context.Projects
