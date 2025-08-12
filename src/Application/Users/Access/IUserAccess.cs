ac8a9d511f15ac370b7d8eb67b3c5d380cbfb0de
Erfan <ErfanMelon@outlook.com>
2025-08-12 22:14:59 +0330

    feat: create a method to check if loggedin user can access specific workspace or not

---
+++ src/Application/Users/Access/IUserAccess.cs
@@ -0,0 +1,7 @@
+﻿using Domain.Subscriptions;
+
+namespace Application.Users.Access;
+internal interface IUserAccess
+{
+    Task<bool> IsAuthenticatedAsync(Guid workspaceId, Role? role = null);
+}
---
+++ src/Application/Users/Access/UserAccess.cs
@@ -0,0 +1,25 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Data;
+using Domain.Subscriptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Users.Access;
+
+internal sealed class UserAccess(IApplicationDbContext context, IUserContext userContext) : IUserAccess
+{
+    public async Task<bool> IsAuthenticatedAsync(Guid workspaceId, Role? role = null)
+    {
+        IQueryable<TeamMember> accessCheckQuery = context.Members
+            .AsNoTracking()
+            .Where(x => x.UserId == userContext.UserId)
+            .Where(x => x.WorkspaceId == workspaceId);
+
+        if (role.HasValue)
+        {
+            accessCheckQuery = accessCheckQuery.Where(x => x.Role == role);
+        }
+        bool isAuthorize = await accessCheckQuery.AnyAsync();
+
+        return isAuthorize;
+    }
+}
