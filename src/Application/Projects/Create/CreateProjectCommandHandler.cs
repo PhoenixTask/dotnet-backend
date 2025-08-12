783c9c7e8798cb713f131f6b867520f08e3516c8
Erfan <ErfanMelon@outlook.com>
2025-08-12 22:28:26 +0330

    fix: remove access check from create project

--- src/Application/Projects/Create/CreateProjectCommandHandler.cs
+++ src/Application/Projects/Create/CreateProjectCommandHandler.cs
@@ -1,33 +1,22 @@
-﻿using Application.Abstractions.Authentication;
-using Application.Abstractions.Data;
+﻿using Application.Abstractions.Data;
 using Application.Abstractions.Messaging;
-using Application.Users.AccessAction;
 using Domain.Projects;
-using Domain.Subscriptions;
 using Domain.Workspaces;
-using MediatR;
 using Microsoft.EntityFrameworkCore;
 using SharedKernel;
 
 namespace Application.Projects.Create;
 
 internal sealed class CreateProjectCommandHandler
-    (IApplicationDbContext context, IUserContext userContext,ISender sender) : ICommandHandler<CreateProjectCommand, Guid>
+    (IApplicationDbContext context) : ICommandHandler<CreateProjectCommand, Guid>
 {
     public async Task<Result<Guid>> Handle(CreateProjectCommand request, CancellationToken cancellationToken)
     {
-        Workspace? workspace = await context.Workspaces.SingleOrDefaultAsync(x=>x.Id == request.WorkspaceId, cancellationToken);
+        Workspace? workspace = await context.Workspaces.SingleOrDefaultAsync(x => x.Id == request.WorkspaceId, cancellationToken);
 
         if (workspace is null)
         {
             return Result.Failure<Guid>(WorkspaceErrors.NotFound(request.WorkspaceId));
-        }
-
-        UserAccessCommand accessRequest = new(userContext.UserId, workspace.Id, typeof(Workspace), ProjectRole.Manager);
-        Result accessResult = await sender.Send(accessRequest, cancellationToken);
-        if (accessResult.IsFailure)
-        {
-            return Result.Failure<Guid>(accessResult.Error);
         }
 
         var project = new Project
