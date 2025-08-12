33dbfb688e8c73f3a52caa29c18f3eaa099a1bf2
Erfan <ErfanMelon@outlook.com>
2025-08-12 22:16:10 +0330

    fix: join owner of workspace when new workspace created

--- src/Application/Workspaces/Create/CreateWorkspaceCommandHandler.cs
+++ src/Application/Workspaces/Create/CreateWorkspaceCommandHandler.cs
@@ -1,11 +1,13 @@
-﻿using Application.Abstractions.Data;
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Data;
 using Application.Abstractions.Messaging;
+using Domain.Subscriptions;
 using Domain.Workspaces;
 using SharedKernel;
 
 namespace Application.Workspaces.Create;
 
-internal sealed class CreateWorkspaceCommandHandler(IApplicationDbContext context)
+internal sealed class CreateWorkspaceCommandHandler(IApplicationDbContext context, IUserContext userContext)
     : ICommandHandler<CreateWorkspaceCommand, Guid>
 {
     public async Task<Result<Guid>> Handle(CreateWorkspaceCommand request, CancellationToken cancellationToken)
@@ -16,6 +18,15 @@ internal sealed class CreateWorkspaceCommandHandler(IApplicationDbContext contex
             Color = request.Color.Trim(),
         };
         await context.Workspaces.AddAsync(workspace, cancellationToken);
+
+        var owner = new TeamMember
+        {
+            UserId = userContext.UserId,
+            Role = Role.Owner,
+            Workspace = workspace
+        };
+        await context.Members.AddAsync(owner, cancellationToken);
+
         await context.SaveChangesAsync(cancellationToken);
         return workspace.Id;
     }
