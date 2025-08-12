a5756ba2e38d0202a1fd01b5dfe18cf92db4b580
Erfan <ErfanMelon@outlook.com>
2025-08-12 22:23:11 +0330

    fix: remove workspace if user has permission

--- src/Application/Workspaces/Delete/DeleteWorkspaceCommandHandler.cs
+++ src/Application/Workspaces/Delete/DeleteWorkspaceCommandHandler.cs
@@ -1,13 +1,14 @@
-﻿using Application.Abstractions.Authentication;
-using Application.Abstractions.Data;
+﻿using Application.Abstractions.Data;
 using Application.Abstractions.Messaging;
+using Application.Users.Access;
+using Domain.Subscriptions;
 using Domain.Workspaces;
 using Microsoft.EntityFrameworkCore;
 using SharedKernel;
 
 namespace Application.Workspaces.Delete;
 
-internal sealed class DeleteWorkspaceCommandHandler(IApplicationDbContext context, IUserContext userContext)
+internal sealed class DeleteWorkspaceCommandHandler(IApplicationDbContext context, IUserAccess userAccess)
     : ICommandHandler<DeleteWorkspaceCommand>
 {
     public async Task<Result> Handle(DeleteWorkspaceCommand request, CancellationToken cancellationToken)
@@ -16,16 +17,18 @@ internal sealed class DeleteWorkspaceCommandHandler(IApplicationDbContext contex
             .Include(x => x.Projects)
             .ThenInclude(x => x.Boards)
             .ThenInclude(x => x.Tasks)
-            .SingleOrDefaultAsync(x => x.Id == request.Id && x.CreatedById == userContext.UserId,
-            cancellationToken);
+            .SingleOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
 
-        if (workspace is null)
+        bool hasOwnerAccess = await userAccess.IsAuthenticatedAsync(request.Id, Role.Owner);
+
+        if (workspace is null || !hasOwnerAccess)
         {
             return Result.Failure(WorkspaceErrors.NotFound(request.Id));
         }
-        context.Workspaces.Remove(workspace);
-        int rowAffect = await context.SaveChangesAsync(cancellationToken);
 
-        return rowAffect > 0 ? Result.Success() : Result.Failure(Error.None);
+        context.Workspaces.Remove(workspace);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
     }
 }
