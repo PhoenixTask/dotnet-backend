d41201c15cf8908e5baf605cb6b8d65225dd0710
Erfan <ErfanMelon@outlook.com>
2025-08-12 22:28:39 +0330

    fix: remove access check from get board

--- src/Application/Boards/GetById/GetBoardByIdQueryHandler.cs
+++ src/Application/Boards/GetById/GetBoardByIdQueryHandler.cs
@@ -1,16 +1,13 @@
-﻿using Application.Abstractions.Authentication;
-using Application.Abstractions.Data;
+﻿using Application.Abstractions.Data;
 using Application.Abstractions.Messaging;
 using Application.Boards.Get;
-using Application.Users.AccessAction;
 using Domain.Projects;
-using MediatR;
 using Microsoft.EntityFrameworkCore;
 using SharedKernel;
 
 namespace Application.Boards.GetById;
 
-internal sealed class GetBoardByIdQueryHandler(IUserContext userContext, ISender sender, IApplicationDbContext context) : IQueryHandler<GetBoardByIdQuery, BoardResponse>
+internal sealed class GetBoardByIdQueryHandler(IApplicationDbContext context) : IQueryHandler<GetBoardByIdQuery, BoardResponse>
 {
     public async Task<Result<BoardResponse>> Handle(GetBoardByIdQuery request, CancellationToken cancellationToken)
     {
@@ -20,13 +17,6 @@ internal sealed class GetBoardByIdQueryHandler(IUserContext userContext, ISender
         if (!boardExists)
         {
             return Result.Failure<BoardResponse>(BoardErrors.NotFound(request.BoardId));
-        }
-
-        UserAccessCommand accessRequest = new(userContext.UserId, request.BoardId, typeof(Board));
-        Result accessResult = await sender.Send(accessRequest, cancellationToken);
-        if (accessResult.IsFailure)
-        {
-            return Result.Failure<BoardResponse>(accessResult.Error);
         }
 
         return await context.Boards
