adb5221126085165b2ebd843c80c817ffa7da8e7
Erfan <ErfanMelon@outlook.com>
2025-08-12 22:27:14 +0330

    fix: remove access check from get task

--- src/Application/Tasks/GetById/GetTaskByIdQueryHandler.cs
+++ src/Application/Tasks/GetById/GetTaskByIdQueryHandler.cs
@@ -1,17 +1,14 @@
 ﻿using System.Globalization;
-using Application.Abstractions.Authentication;
 using Application.Abstractions.Data;
 using Application.Abstractions.Messaging;
 using Application.Tasks.Get;
-using Application.Users.AccessAction;
 using Domain.Tasks;
-using MediatR;
 using Microsoft.EntityFrameworkCore;
 using SharedKernel;
 
 namespace Application.Tasks.GetById;
 
-internal sealed class GetTaskByIdQueryHandler(IUserContext userContext, ISender sender, IApplicationDbContext context) : IQueryHandler<GetTaskByIdQuery, TaskResponse>
+internal sealed class GetTaskByIdQueryHandler(IApplicationDbContext context) : IQueryHandler<GetTaskByIdQuery, TaskResponse>
 {
     public async Task<Result<TaskResponse>> Handle(GetTaskByIdQuery request, CancellationToken cancellationToken)
     {
@@ -21,13 +18,6 @@ internal sealed class GetTaskByIdQueryHandler(IUserContext userContext, ISender 
         if (!taskExists)
         {
             return Result.Failure<TaskResponse>(TaskErrors.NotFound(request.TaskId));
-        }
-
-        UserAccessCommand accessRequest = new(userContext.UserId, request.TaskId, typeof(Domain.Tasks.Task));
-        Result accessResult = await sender.Send(accessRequest, cancellationToken);
-        if (accessResult.IsFailure)
-        {
-            return Result.Failure<TaskResponse>(accessResult.Error);
         }
 
         return await context.Tasks
