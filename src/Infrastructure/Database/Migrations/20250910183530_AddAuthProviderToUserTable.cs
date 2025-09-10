dda4899018a49568815b406d7ed78ab7c8ab0e87
Erfan <ErfanMelon@outlook.com>
2025-09-10 22:23:17 +0330

    feat: apply migration and add auth provider to user table

---
+++ src/Infrastructure/Database/Migrations/20250910183530_AddAuthProviderToUserTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Database.Migrations;
+
+/// <inheritdoc />
+public partial class AddAuthProviderToUserTable : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<string>(
+            name: "password_hash",
+            schema: "public",
+            table: "users",
+            type: "TEXT",
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "TEXT");
+
+        migrationBuilder.AddColumn<int>(
+            name: "auth_provider",
+            schema: "public",
+            table: "users",
+            type: "INTEGER",
+            nullable: false,
+            defaultValue: 0);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "auth_provider",
+            schema: "public",
+            table: "users");
+
+        migrationBuilder.AlterColumn<string>(
+            name: "password_hash",
+            schema: "public",
+            table: "users",
+            type: "TEXT",
+            nullable: false,
+            defaultValue: "",
+            oldClrType: typeof(string),
+            oldType: "TEXT",
+            oldNullable: true);
+    }
+}
