be4972a06dbf63a311cb03ca4df357dce22f7972
Erfan <ErfanMelon@outlook.com>
2025-09-10 22:21:30 +0330

    feat: create Google Provider and implement it

---
+++ src/Application/Abstractions/Authentication/IGoogleProvider.cs
@@ -0,0 +1,5 @@
+﻿namespace Application.Abstractions.Authentication;
+public interface IGoogleProvider
+{
+    Task<Dictionary<string, string>?> GetClaimsAsync(string accessToken, CancellationToken cancellationToken);
+}
---
+++ src/Infrastructure/Authentication/GoogleProvider.cs
@@ -0,0 +1,36 @@
+﻿using Application.Abstractions.Authentication;
+using Google.Apis.Auth;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Authentication;
+internal class GoogleProvider(IConfiguration configuration) : IGoogleProvider
+{
+    public async Task<Dictionary<string, string>?> GetClaimsAsync(string accessToken, CancellationToken cancellationToken)
+    {
+        try
+        {
+            GoogleJsonWebSignature.Payload payload = await GoogleJsonWebSignature.ValidateAsync(accessToken,
+         new GoogleJsonWebSignature.ValidationSettings
+         {
+             Audience = [configuration["Google:ClientId"]]
+         });
+            var result = new Dictionary<string, string>
+        {
+            { "Scope", payload.Scope },
+            { "Prn", payload.Prn },
+            { "HostedDomain", payload.HostedDomain },
+            { "Email", payload.Email },
+            { "EmailVerified", payload.EmailVerified.ToString() },
+            { "GivenName", payload.GivenName },
+            { "FamilyName", payload.FamilyName },
+            { "Picture", payload.Picture },
+            { "Locale", payload.Locale }
+        };
+            return result;
+        }
+        catch (InvalidJwtException)
+        {
+            return null;
+        }
+    }
+}
