58bac1602787eff58545a2520c4e916c41284b06
Erfan <ErfanMelon@outlook.com>
2025-09-10 22:28:01 +0330

    feat: create login with google endpoint

---
+++ src/Web.Api/Endpoints/Users/GoogleLogin.cs
@@ -0,0 +1,27 @@
+﻿using Application.Users.Login;
+using Application.Users.LoginGoogle;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Users;
+
+internal sealed class GoogleLogin : IEndpoint
+{
+    public sealed record Request(string TokenId);
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("user/google-login", async (Request request, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new LoginGoogleCommand(request.TokenId);
+
+            Result<LoginResponse> result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+       .WithName("Login User By Google Provider")
+       .WithSummary("Authenticate user with google")
+       .WithTags(Tags.Users);
+    }
+}
