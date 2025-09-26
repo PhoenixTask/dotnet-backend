9983c846b0e144671c154aab72721d930dcb0361
Erfan <ErfanMelon@outlook.com>
2025-09-26 12:32:19 +0330

    feat: set http only cookie in login

--- src/Web.Api/Endpoints/Users/Login.cs
+++ src/Web.Api/Endpoints/Users/Login.cs
@@ -24,5 +24,23 @@ internal sealed class Login : IEndpoint
         .WithName("Login User v2")
         .WithSummary("Authenticate user with username and password (returns refresh token)")
         .WithTags(Tags.Users);
+
+        app.MapPost("user/login", async (Request request, ISender sender, HttpContext context, CancellationToken cancellationToken) =>
+        {
+            var command = new LoginCommand(request.Username, request.Password);
+
+            Result<LoginResponse> result = await sender.Send(command, cancellationToken);
+
+            if (result.IsFailure)
+            {
+                return CustomResults.Problem(result);
+            }
+            CookieService.SetToken(result.Value.Token, result.Value.RefreshToken, context);
+            return Results.NoContent();
+        })
+        .HasApiVersion(3)
+        .WithName("Login User v3")
+        .WithSummary("Authenticate user with username and password (http only cookie)")
+        .WithTags(Tags.Users);
     }
 }
