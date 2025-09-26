7669189d2b95c443facc744f9939f7d6b51e2cd3
Erfan <ErfanMelon@outlook.com>
2025-09-26 12:31:55 +0330

    feat: revoke refresh token when logout

---
+++ src/Application/Users/RevokeRefreshToken/RevokeRefreshTokenCommand.cs
@@ -0,0 +1,4 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.RevokeRefreshToken;
+public sealed record RevokeRefreshTokenCommand(string Refresh) : ICommand;
---
+++ src/Application/Users/RevokeRefreshToken/RevokeRefreshTokenCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Application.Users.RevokeRefreshToken;
+
+internal sealed class RevokeRefreshTokenCommandHandler(IApplicationDbContext context, IUserContext userContext) : ICommandHandler<RevokeRefreshTokenCommand>
+{
+    public async Task<Result> Handle(RevokeRefreshTokenCommand request, CancellationToken cancellationToken)
+    {
+        UserToken? token = await context.RefreshTokens
+            .Where(x => x.UserId == userContext.UserId)
+            .Where(x => x.TokenType == TokenType.RefreshToken)
+            .SingleOrDefaultAsync(x => x.Token == request.Refresh, cancellationToken);
+
+        if (token is null)
+        {
+            return Result.Failure(UserErrors.Unauthorized);
+        }
+
+        context.RefreshTokens.Remove(token);
+        await context.SaveChangesAsync(cancellationToken);
+        return Result.Success();
+    }
+}
