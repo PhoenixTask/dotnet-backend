27d682a973884108815b4d3589362c8451cc275f
Erfan <ErfanMelon@outlook.com>
2025-09-26 12:32:41 +0330

    feat: set http only cookie in login with refresh token

--- src/Web.Api/Endpoints/Users/RefreshToken.cs
+++ src/Web.Api/Endpoints/Users/RefreshToken.cs
@@ -10,6 +10,7 @@ namespace Web.Api.Endpoints.Users;
 internal sealed class RefreshToken : IEndpoint
 {
     public sealed record Request(Guid UserId, string RefreshToken);
+    public sealed record Request2(Guid UserId);
 
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
@@ -24,5 +25,23 @@ internal sealed class RefreshToken : IEndpoint
         .WithName("Refresh Token")
         .WithSummary("Refresh user authentication token")
         .WithTags(Tags.Users);
+
+        app.MapPost("user/refresh-token", async (Request2 request, ISender sender, HttpContext context, CancellationToken cancellationToken) =>
+        {
+            var command = new RefreshTokenCommnad(request.UserId, CookieService.GetRefreshToken(context));
+
+            Result<LoginResponse> result = await sender.Send(command, cancellationToken);
+
+            if (result.IsFailure)
+            {
+                return CustomResults.Problem(result);
+            }
+            CookieService.SetToken(result.Value.Token, result.Value.RefreshToken, context);
+            return Results.NoContent();
+        })
+            .WithName("Refresh Token (http only cookie)")
+            .HasApiVersion(2)
+            .WithSummary("Refresh user authentication token")
+            .WithTags(Tags.Users);
     }
 }
